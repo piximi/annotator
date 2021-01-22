045785c1743fc2eb17d690da4d1e2d79f654b9c5
Allen Goodman <allen.goodman@icloud.com>
2021-01-22 16:45:29 -0500

    cleanup

--- src/store/selectors/index.ts
+++ src/store/selectors/index.ts
@@ -1,27 +1,15 @@
 export { categoriesSelector } from "./categoriesSelector";
-export { categorizedImagesSelector } from "./categorizedImagesSelector";
-export { classifierSelector } from "./classifierSelector";
-export { compileOptionsSelector } from "./compileOptionsSelector";
-export { compiledSelector } from "./compiledSelector";
 export { createdCategoriesCountSelector } from "./createdCategoriesCountSelector";
 export { createdCategoriesSelector } from "./createdCategoriesSelector";
-export { dataSelector } from "./dataSelector";
-export { epochsSelector } from "./epochsSelector";
-export { fitOptionsSelector } from "./fitOptionsSelector";
-export { fittedSelector } from "./fittedSelector";
-export { generatorOptionsSelector } from "./generatorOptionsSelector";
-export { imageViewerImageSelector } from "./imageViewerImageSelector";
-export { imageViewerOperationSelector } from "./imageViewerOperationSelector";
-export { imageViewerSelectionModeSelector } from "./imageViewerSelectionModeSelector";
-export { imagesSelector } from "./imagesSelector";
-export { lossHistorySelector } from "./lossHistorySelector";
-export { openedSelector } from "./openedSelector";
-export { openingSelector } from "./openingSelector";
+export { imageViewerBrightnessSelector} from "./imageViewerBrightnessSelector"
+export { imageViewerContrastSelector} from "./imageViewerContrastSelector"
+export { imageViewerExposureSelector} from "./imageViewerExposureSelector"
+export { imageViewerHueSelector} from "./imageViewerHueSelector"
+export { imageViewerImageSelector} from "./imageViewerImageSelector"
+export { imageViewerOperationSelector} from "./imageViewerOperationSelector"
+export { imageViewerSelectionModeSelector} from "./imageViewerSelectionModeSelector"
+export { imageViewerZoomModeSelector} from "./imageViewerZoomModeSelector"
 export { projectSelector } from "./projectSelector";
-export { selectedImagesSelector } from "./selectedImagesSelector";
-export { trainingPercentageSelector } from "./trainingPercentageSelector";
 export { unknownCategorySelector } from "./unknownCategorySelector";
-export { validationDataSelector } from "./validationDataSelector";
-export { validationLossHistorySelector } from "./validationLossHistorySelector";
-export { validationPercentageSelector } from "./validationPercentageSelector";
-export { visibleImagesSelector } from "./visibleImagesSelector";
+export {imageViewerSaturationSelector } from "./imageViewerSaturationSelector"
+export {imageViewerVibranceSelector } from "./imageViewerVibranceSelector"

f44251b210fd52d0e85c577698d212c24a5c5061
Allen Goodman <allen.goodman@icloud.com>
2021-01-22 16:30:20 -0500

    cleanup

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import reportWebVitals from './reportWebVitals';
-import {ImageViewer} from "./components/ImageViewer";
+import {ImageViewer} from "./components";
 import {Provider} from "react-redux";
 import {productionStore} from "./store";
 

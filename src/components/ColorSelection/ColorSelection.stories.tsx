f0db6f34a1ec32772296588a9b7393fe4cf8b24e
David Stirling <daves1990@hotmail.co.uk>
2021-01-25 11:17:36 -0500

    Flood implementation

--- src/components/ColorSelection/ColorSelection.stories.tsx
+++ src/components/ColorSelection/ColorSelection.stories.tsx
@@ -2,6 +2,7 @@ import React, {ComponentProps} from "react";
 import {Meta, Story} from "@storybook/react/types-6-0";
 import {Image} from "../../types/Image";
 import {ColorSelection} from "./ColorSelection";
+import { Category } from "../../types/Category";
 
 export default {
   component: ColorSelection,
@@ -9,7 +10,7 @@ export default {
 } as Meta;
 
 const Template: Story<ComponentProps<typeof ColorSelection>> = (args) => (
-  <ColorSelection {...args} />
+    <ColorSelection {...args} />
 );
 
 export const Default = Template.bind({});
@@ -22,6 +23,14 @@ const image: Image = {
   src: "https://picsum.photos/id/237/512/512",
 };
 
+const category: Category = {
+  id: "",
+  color: "#9c27b0",
+  name: "TestCat",
+  visible: true,
+};
+
 Default.args = {
   image: image,
+  category: category,
 };

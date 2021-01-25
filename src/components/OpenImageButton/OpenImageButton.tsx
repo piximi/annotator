f5b7346c10641bc31d508dd53c52e2cd055fe984
Allen Goodman <allen.goodman@icloud.com>
2021-01-25 13:43:12 -0500

    OpenImageMenu

--- src/components/OpenImageButton/OpenImageButton.tsx
+++ src/components/OpenImageButton/OpenImageButton.tsx
@@ -1,18 +1,24 @@
-import React from "react";
-import {useStyles} from "./OpenImageButton.css";
-import {Button} from "@material-ui/core";
+import Button from "@material-ui/core/Button";
 import CloudUploadIcon from "@material-ui/icons/CloudUpload";
+import Menu from "@material-ui/core/Menu";
+import MenuItem from "@material-ui/core/MenuItem";
+import React from "react";
 import Tooltip from "@material-ui/core/Tooltip";
 import {Shape} from "../../types/Shape";
 import {setImageViewerImage} from "../../store/slices";
 import {useDispatch} from "react-redux";
+import {useStyles} from "./OpenImageButton.css";
 
-export const OpenImageButton = () => {
+type ComputerMenuItemProps = {
+  onClose: () => void;
+}
+
+const ComputerMenuItem = ({onClose}: ComputerMenuItemProps) => {
   const dispatch = useDispatch();
 
-  const classes = useStyles();
-
   const onChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    onClose();
+
     event.persist();
 
     if (event.currentTarget.files) {
@@ -56,24 +62,72 @@ export const OpenImageButton = () => {
     }
   };
 
+
+  return (
+    <MenuItem component="label">
+      Computer
+      <input
+        accept="image/*"
+        hidden
+        id="open-image"
+        onChange={onChange}
+        type="file"
+      />
+    </MenuItem>
+  );
+}
+
+type ExampleMenuItemProps = {
+  onClose: () => void;
+}
+
+const ExampleMenuItem = ({onClose}: ExampleMenuItemProps) => {
+  const onClick = () => {
+    onClose();
+  }
+
+  return (
+    <MenuItem onClick={onClick}>Example</MenuItem>
+  );
+}
+
+type OpenImageMenuProps = {
+  anchorEl: null | HTMLElement;
+  onClose: () => void;
+}
+
+const OpenImageMenu = ({anchorEl, onClose}: OpenImageMenuProps) => {
+  return (
+    <Menu anchorEl={anchorEl} keepMounted onClose={onClose} open={Boolean(anchorEl)}>
+      <ComputerMenuItem onClose={onClose}/>
+
+      <ExampleMenuItem onClose={onClose}/>
+    </Menu>
+  )
+}
+
+export const OpenImageButton = () => {
+  const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
+
+  const classes = useStyles();
+
+  const onClick = (event: React.MouseEvent<HTMLButtonElement>) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const onClose = () => {
+    setAnchorEl(null);
+  };
+
   return (
     <React.Fragment>
       <Tooltip title="Open image">
-        <Button
-          className={classes.button}
-          component="label"
-          startIcon={<CloudUploadIcon />}
-        >
+        <Button className={classes.button} startIcon={<CloudUploadIcon />} onClick={onClick}>
           Open image
-          <input
-            accept="image/*"
-            hidden
-            type="file"
-            id="open-image"
-            onChange={onChange}
-          />
         </Button>
       </Tooltip>
+
+      <OpenImageMenu anchorEl={anchorEl} onClose={onClose}/>
     </React.Fragment>
   );
 };

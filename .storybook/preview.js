8dcff50325cb9b14330eb04c773dae92d4a931f5
Allen Goodman <allen.goodman@icloud.com>
2021-01-25 11:27:18 -0500

    bug fixes

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,3 +1,14 @@
+import {Provider} from "react-redux";
+import React from "react";
+import {productionStore} from "../src/store";
+
+export const decorators = [
+  (Story) => (
+      <Provider store={productionStore}>
+        <Story />
+      </Provider>
+  ),
+];
 
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },

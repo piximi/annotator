6e0f4b83f66a406fdc01704ae88dfb69d18f1d79
Allen Goodman <allen.goodman@icloud.com>
2021-01-26 14:16:23 -0500

    selecting and selection

--- src/image/selection/EllipticalSelection.ts
+++ src/image/selection/EllipticalSelection.ts
@@ -6,15 +6,27 @@ export class EllipticalSelection extends Selection {
   public radius?: { x: number; y: number };
 
   public onMouseDown(position: { x: number; y: number }): void {
+    if (this.selected) return;
+
     this.origin = position;
+
+    this.selecting = true;
   }
 
   public onMouseMove(position: { x: number; y: number }): void {
+    if (this.selected) return;
+
     this.resize(position);
   }
 
   public onMouseUp(position: { x: number; y: number }): void {
+    if (this.selected || !this.selecting) return;
+
     this.resize(position);
+
+    this.selected = true;
+
+    this.selecting = false;
   }
 
   private resize(position: { x: number; y: number }) {
--- src/image/selection/RectangularSelection.ts
+++ src/image/selection/RectangularSelection.ts
@@ -7,15 +7,27 @@ export class RectangularSelection extends Selection {
   public origin?: { x: number; y: number };
 
   public onMouseDown(position: { x: number; y: number }): void {
+    if (this.selected) return;
+
     this.origin = position;
+
+    this.selecting = true;
   }
 
   public onMouseMove(position: { x: number; y: number }): void {
+    if (this.selected) return;
+
     this.resize(position);
   }
 
   public onMouseUp(position: { x: number; y: number }): void {
+    if (this.selected || !this.selecting) return;
+
     this.resize(position);
+
+    this.selected = true;
+
+    this.selecting = false;
   }
 
   private resize(position: { x: number; y: number }) {

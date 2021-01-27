48d409fe71d2080e2c34bcaf7cc4704f49fcb8dd
alucas <alicelucas93@gmail.com>
2021-01-27 14:57:51 -0500

    Object Selection class

--- src/image/selection/ObjectSelectionOperator.ts
+++ src/image/selection/ObjectSelectionOperator.ts
@@ -1,6 +1,25 @@
 import { SelectionOperator } from "./SelectionOperator";
+import * as tensorflow from "@tensorflow/tfjs";
 
 export class ObjectSelectionOperator extends SelectionOperator {
+
+  model?: tensorflow.LayersModel;
+
+  constructor() {
+    super()
+    const pathname =
+        "https://raw.githubusercontent.com/zaidalyafeai/HostedModels/master/unet-128/model.json";
+    tensorflow.loadLayersModel(pathname).then( (graph) => {
+      const optimizer = tensorflow.train.adam();
+      graph.compile({
+        optimizer: optimizer,
+        loss: "categoricalCrossentropy",
+        metrics: ["accuracy"],
+      });
+      this.model = graph
+    })
+  }
+
   get boundingBox(): [number, number, number, number] | undefined {
     return undefined;
   }

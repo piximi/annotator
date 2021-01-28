7deb137a952072a8672b25ba0755213e81c25e32
Allen Goodman <allen.goodman@icloud.com>
2021-01-28 12:03:35 -0500

    PolygonalSelectionOperator

--- src/image/selection/PolygonalSelectionOperator.ts
+++ src/image/selection/PolygonalSelectionOperator.ts
@@ -1,6 +1,16 @@
 import { SelectionOperator } from "./SelectionOperator";
 
 export class PolygonalSelectionOperator extends SelectionOperator {
+  anchor?: { x: number; y: number };
+
+  connected: boolean = false;
+
+  origin?: { x: number; y: number };
+
+  points: Array<number> = [];
+
+  shape: Array<number> = [];
+
   get boundingBox(): [number, number, number, number] | undefined {
     return undefined;
   }
@@ -11,9 +21,52 @@ export class PolygonalSelectionOperator extends SelectionOperator {
 
   deselect() {}
 
-  onMouseDown(position: { x: number; y: number }) {}
+  onMouseDown(position: { x: number; y: number }) {
+    if (this.connected) {
+      this.selected = true;
+      this.selecting = false;
 
-  onMouseMove(position: { x: number; y: number }) {}
+      this.shape = this.points;
+
+      this.points = [];
+
+      this.origin = undefined;
+    } else {
+      if (this.points && this.points.length === 0) {
+        this.selecting = true;
+
+        if (!this.origin) {
+          this.origin = position;
+        }
+      }
+    }
+  }
+
+  onMouseMove(position: { x: number; y: number }) {
+    if (this.anchor) {
+      this.points.pop();
+
+      this.points = [
+        ...this.points,
+        this.anchor.x,
+        this.anchor.y,
+        position.x,
+        position.y,
+      ];
+    } else {
+      if (this.origin) {
+        this.points.pop();
+
+        this.points = [
+          ...this.points,
+          this.origin.x,
+          this.origin.y,
+          position.x,
+          position.y,
+        ];
+      }
+    }
+  }
 
   onMouseUp(position: { x: number; y: number }) {}
 

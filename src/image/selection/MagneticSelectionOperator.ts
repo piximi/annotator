6ac1b5733e1350257d231e2a24aadd62dbe537f5
David Stirling <daves1990@hotmail.co.uk>
2021-01-28 17:05:02 -0500

    Start implementing MagneticSelection as a class

--- src/image/selection/MagneticSelectionOperator.ts
+++ src/image/selection/MagneticSelectionOperator.ts
@@ -1,6 +1,22 @@
 import { SelectionOperator } from "./SelectionOperator";
+import * as _ from "underscore";
+import {createPathFinder, makeGraph, PiximiGraph} from "../GraphHelper";
+import {getIdx} from "../imageHelper";
+import {transformCoordinatesToStrokes} from "../pathfinder/PathFinder";
 
 export class MagneticSelectionOperator extends SelectionOperator {
+  graph? : PiximiGraph;
+  scalingFactor : number = 0.5;
+  downsizedWidth : number = 0;
+  pathFinder?: { find: (fromId: number, toId: number) => any; };
+  annotation?: { points: Array<number> };
+  previousStroke : Array<{ points: Array<number> }> = [];
+  strokes : Array<{ points: Array<number> }> = [];
+  startPosition : { x: number; y: number; } = {x: 0, y: 0};
+  canClose : boolean = false;
+  currentPosition? : { x: number; y: number };
+  anchor? : { x: number; y: number; }; // Not sure this is even necessary
+
   get boundingBox(): [number, number, number, number] | undefined {
     return undefined;
   }
@@ -9,13 +25,167 @@ export class MagneticSelectionOperator extends SelectionOperator {
     return undefined;
   }
 
-  deselect() {}
+  deselect() {
+    this.selected = false;
+    this.selecting = false;
 
-  onMouseDown(position: { x: number; y: number }) {}
+    this.center = undefined;
+    this.origin = undefined;
+    this.radius = undefined;
+  }
 
-  onMouseMove(position: { x: number; y: number }) {}
+  onMouseDown(position: { x: number; y: number }) {
+    if (this.selected) {
+      return;
+    }
 
-  onMouseUp(position: { x: number; y: number }) {}
+    this.currentPosition = position;
 
-  select(category: number) {}
+    if (magnetConnected(this.currentPosition)) {
+      this.annotation = {
+        points: _.flatten(
+            this.strokes.map((stroke) => stroke.points)
+        ),
+      };
+
+      this.selected = true;
+
+      this.selecting = false;
+
+    } else {
+      this.selecting = true;
+
+      this.startPosition = this.currentPosition;
+
+      if (this.strokes.length > 0) {
+        this.anchor = this.currentPosition;
+
+        this.previousStroke = [...this.previousStroke, ...this.strokes,];
+      }
+    }
+
+
+  }
+
+  onMouseMove(position: { x: number; y: number }) {
+    if (this.selected || !this.selecting) {
+      return;
+    }
+
+      this.currentPosition = position;
+
+      if (this.currentPosition) {
+        if (
+            !this.canClose &&
+            !this.isInside(
+                magneticSelectionStartingAnchorCircleRef,
+                this.currentPosition
+            )
+        ) {
+          this.canClose = true;
+        }
+
+        // let startPosition;
+        if ( this.pathFinder && this.startPosition ) {
+          const pathCoords = this.pathFinder.find(
+              getIdx(this.downsizedWidth, 1)(
+                  Math.floor(this.startPosition.x *this.scalingFactor),
+                  Math.floor(this.startPosition.y * this.scalingFactor),
+                  0
+              ),
+              getIdx(this.downsizedWidth, 1)(
+                  Math.floor(this.currentPosition.x * this.scalingFactor),
+                  Math.floor(this.currentPosition.y * this.scalingFactor),
+                  0
+              )
+          );
+
+          this.strokes = transformCoordinatesToStrokes(pathCoords);
+        }
+      }
+  };
+
+  onMouseUp(position: { x: number; y: number }) {
+    if (this.selected || !this.selecting) {
+      return;
+    }
+
+    this.currentPosition = position;
+
+    if (this.magnetConnected(this.currentPosition)) {
+      if (this.startPosition) {
+        const stroke = {
+          points: [
+            this.currentPosition.x,
+            this.currentPosition.y,
+            this.startPosition.x,
+            this.startPosition.y,
+          ],
+        };
+
+        this.strokes = [...this.strokes, stroke];
+      }
+
+      const stroke = {
+        points: _.flatten(
+            this.strokes.map((stroke) => stroke.points)
+        ),
+      };
+
+      this.selected = true;
+
+      this.selecting = false;
+
+      this.annotation = stroke;
+
+      this.strokes = [];
+    } else {
+      if (this.strokes.length > 0) {
+        this.anchor = position;
+
+        this.startPosition = this.currentPosition;
+
+        this.previousStroke = [...this.previousStroke, ...this.strokes];
+      } else {
+        this.startPosition = this.currentPosition;
+      }
+    }
+  };
+
+  select(category: number) {
+    if (!this.image) {
+      console.log("Error: no image");
+      return
+    }
+    if (!this.graph) {
+      console.log("Creating graph");
+      const grey = this.image!.grey();
+      const edges = grey.sobelFilter();
+      this.downsizedWidth = this.image!.width * this.scalingFactor;
+      const downsized = edges.resize({factor: this.scalingFactor});
+      this.graph = makeGraph(downsized.data, downsized.height, downsized.width);
+      console.log("Creating pathfinder");
+      this.pathFinder = createPathFinder(
+        this.graph,
+        this.downsizedWidth,
+        this.scalingFactor,
+      );
+    }
+
+    if (!this.boundingBox || !this.mask) return;
+    this.selection = {
+      boundingBox: this.boundingBox,
+      categoryId: category,
+      mask: this.mask,
+    };
+
+  }
+
+  magnetConnected(position: { x: number; y: number })  {
+    const inside = this.isInside(magneticSelectionStartingAnchorCircleRef, position);
+    if (this.strokes && this.strokes.length > 0) {
+      return inside && this.canClose;
+    }
+  };
+
 }
--- src/image/selection/SelectionOperator.ts
+++ src/image/selection/SelectionOperator.ts
@@ -1,5 +1,7 @@
 import { ImageViewerSelection } from "../../types/ImageViewerSelection";
 import * as ImageJS from "image-js";
+import React from "react";
+import {Circle} from "konva/types/shapes/Circle";
 
 export abstract class SelectionOperator {
   image?: ImageJS.Image;
@@ -27,4 +29,32 @@ export abstract class SelectionOperator {
   abstract onMouseUp(position: { x: number; y: number }): void;
 
   abstract select(category: number): void;
-}
+
+  isInside = (
+    startingAnchorCircleRef: React.RefObject<Circle>,
+    position: { x: number; y: number }
+) => {
+  if (
+      startingAnchorCircleRef &&
+      startingAnchorCircleRef.current
+  ) {
+    let rectangle = startingAnchorCircleRef.current.getClientRect();
+
+    const transform = imageRef.current.getAbsoluteTransform().copy();
+    transform.invert();
+    const transformedRectangle = transform.point({
+      x: rectangle.x,
+      y: rectangle.y,
+    });
+    return (
+        transformedRectangle.x <= position.x &&
+        position.x <= transformedRectangle.x + rectangle.width &&
+        transformedRectangle.y <= position.y &&
+        position.y <= transformedRectangle.y + rectangle.height
+    );
+  } else {
+    return false;
+  }
+  };
+
+  }

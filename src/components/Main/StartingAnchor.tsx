833301a065c9e25545e960d27ef7fa73f880141b
alucas <alicelucas93@gmail.com>
2021-01-29 11:55:51 -0500

    saving work on start anchor

--- src/components/Main/StartingAnchor.tsx
+++ src/components/Main/StartingAnchor.tsx
@@ -3,26 +3,29 @@ import React, { RefObject } from "react";
 import { Circle } from "konva/types/shapes/Circle";
 
 type StartingAnchorProps = {
-  annotating: boolean;
-  pointRadius: number;
-  position?: { x: number; y: number } | null;
+  selecting: boolean;
+  origin?: { x: number; y: number };
   ref: RefObject<Circle>;
 };
 
 export const StartingAnchor = React.forwardRef<Circle, StartingAnchorProps>(
-  (props, ref) => (
-    <ReactKonva.Circle
-      fill="#000"
-      globalCompositeOperation="source-over"
-      hitStrokeWidth={64}
-      id="start"
-      name="anchor"
-      radius={props.pointRadius}
-      ref={ref}
-      stroke="#FFF"
-      strokeWidth={1}
-      x={props.position?.x}
-      y={props.position?.y}
-    />
-  )
+  (props, ref) => {
+    if (props.selecting && props.origin) {
+      return (
+        <ReactKonva.Circle
+          fill="#000"
+          globalCompositeOperation="source-over"
+          hitStrokeWidth={64}
+          id="start"
+          name="anchor"
+          radius={3}
+          ref={ref}
+          stroke="#FFF"
+          strokeWidth={1}
+          x={props.origin.x}
+          y={props.origin.y}
+        />
+      );
+    }
+  }
 );

1d902a1981204766756c8c91c1dc3e0eaa9ec9b0
David Stirling <daves1990@hotmail.co.uk>
2021-01-29 16:03:06 -0500

    Start moving colorselection to class

--- src/image/selection/ColorSelectionOperator.ts
+++ src/image/selection/ColorSelectionOperator.ts
@@ -1,6 +1,13 @@
 import { SelectionOperator } from "./SelectionOperator";
+import {floodPixels, makeFloodMap} from "../flood";
+import * as ImageJS from "image-js";
 
 export class ColorSelectionOperator extends SelectionOperator {
+  overlayData: string = "";
+  initialPosition: { x: number; y: number } = {x: 0, y: 0};
+  tolerance: number = 1;
+  toleranceMap?: ImageJS.Image;
+
   get boundingBox(): [number, number, number, number] | undefined {
     return undefined;
   }
@@ -11,11 +18,50 @@ export class ColorSelectionOperator extends SelectionOperator {
 
   deselect() {}
 
-  onMouseDown(position: { x: number; y: number }) {}
+  onMouseDown(position: { x: number; y: number }) {
+    this.selected = false;
+    this.selecting = true;
+    this.tolerance = 1;
+    this.initialPosition = position;
+    this.toleranceMap = makeFloodMap({
+                  x: Math.floor(position.x),
+                  y: Math.floor(position.y),
+                  image: this.image!,
+                })
+    this.updateOverlay(position);
+  };
 
-  onMouseMove(position: { x: number; y: number }) {}
+  onMouseMove(position: { x: number; y: number }) {
+    if (this.selecting) {
+        const diff = Math.ceil(
+            Math.hypot(
+                position.x - this.initialPosition!.x,
+                position.y - this.initialPosition!.y
+            )
+        );
+        if (diff !== this.tolerance) {
+          this.tolerance = diff;
+          this.updateOverlay(this.initialPosition);
+        }
+    }
+  };
 
-  onMouseUp(position: { x: number; y: number }) {}
+
+  onMouseUp(position: { x: number; y: number }) {
+    this.selected = true;
+    this.selecting = false;
+  }
 
   select(category: number) {}
+
+  private updateOverlay(position: { x: any; y: any }) {
+    this.overlayData = floodPixels({
+      x: Math.floor(position.x),
+      y: Math.floor(position.y),
+      image: this.toleranceMap!,
+      tolerance: this.tolerance,
+      color: "#FFFFFF", // TODO: Add activeCategory.color
+    });
+  };
+
 }

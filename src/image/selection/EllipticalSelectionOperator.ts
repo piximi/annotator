839706c7feee87b49c585db7d62b6054538bdef2
alucas <alicelucas93@gmail.com>
2021-02-09 17:29:40 -0500

    ellipse selection mask

--- src/image/selection/EllipticalSelectionOperator.ts
+++ src/image/selection/EllipticalSelectionOperator.ts
@@ -1,5 +1,8 @@
 import { SelectionOperator } from "./SelectionOperator";
-import {Category} from "../../types/Category";
+import { Category } from "../../types/Category";
+import { slpf } from "../polygon-fill/slpf";
+import * as ImageJS from "image-js";
+import * as _ from "lodash";
 
 export class EllipticalSelectionOperator extends SelectionOperator {
   center?: { x: number; y: number };
@@ -18,7 +21,21 @@ export class EllipticalSelectionOperator extends SelectionOperator {
   }
 
   get mask(): string | undefined {
-    return "mask";
+    const maskImage = new ImageJS.Image({
+      width: this.image.width,
+      height: this.image.height,
+      bitDepth: 8,
+    });
+
+    const connectedPoints = this.convertToPoints();
+
+    if (!connectedPoints) return;
+
+    slpf(connectedPoints, maskImage);
+    debugger;
+
+    // return maskImage.toDataURL();
+    return maskImage.toDataURL();
   }
 
   deselect() {
@@ -52,6 +69,7 @@ export class EllipticalSelectionOperator extends SelectionOperator {
     this.selected = true;
 
     this.selecting = false;
+    const foo = this.mask;
   }
 
   select(category: Category) {
@@ -62,6 +80,46 @@ export class EllipticalSelectionOperator extends SelectionOperator {
       categoryId: category.id,
       mask: this.mask,
     };
+  }
+
+  private convertToPoints() {
+    if (!this.radius || !this.origin || !this.center) return;
+
+    const centerX = Math.round(this.center.x);
+    const centerY = Math.round(this.center.y);
+
+    const connectedPoints: Array<Array<number>> = [];
+    const foo: Array<Array<number>> = [];
+    //first quadrant points
+    for (let y = this.center.y; y < this.center.y + this.radius.y; y++) {
+      const x =
+        this.radius.x *
+          Math.sqrt(
+            1 -
+              ((y - centerY) * (y - centerY)) / (this.radius.y * this.radius.y)
+          ) +
+        centerX;
+      connectedPoints.push([Math.round(x), Math.round(y)]);
+      foo.push([Math.round(x), Math.round(y)]);
+    }
+    //second quadrant points
+    _.reverse(foo).forEach((position: Array<number>) => {
+      let x = 2 * centerX - position[0];
+      connectedPoints.push([x, position[1]]);
+    });
+    //third quadrant points
+    foo.forEach((position: Array<number>) => {
+      let x = 2 * centerX - position[0];
+      let y = 2 * centerY - position[1];
+      connectedPoints.push([x, y]);
+    });
+    //fourth quadant points
+    _.reverse(foo).forEach((position) => {
+      let y = 2 * centerY - position[1];
+      connectedPoints.push([position[0], y]);
+    });
+
+    return connectedPoints;
   }
 
   private resize(position: { x: number; y: number }) {

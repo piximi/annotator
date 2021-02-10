8c3c59637ea5e95f2c771e7c62b6c7b42361f608
Allen Goodman <allen.goodman@icloud.com>
2021-02-10 11:05:02 -0500

    saveAs

--- src/components/ImageViewer/ImageViewerAppBar/ExportButton/ExportButton.tsx
+++ src/components/ImageViewer/ImageViewerAppBar/ExportButton/ExportButton.tsx
@@ -3,11 +3,18 @@ import CloudUploadIcon from "@material-ui/icons/CloudUpload";
 import React from "react";
 import Tooltip from "@material-ui/core/Tooltip";
 import { useStyles } from "./ExportButton.css";
+import { saveAs } from "file-saver";
 
 export const ExportButton = () => {
   const classes = useStyles();
 
-  const onClick = () => {};
+  const onClick = () => {
+    const blob = new Blob(["{ foo: true }"], {
+      type: "text/json;charset=utf-8",
+    });
+
+    saveAs(blob, "foo.json");
+  };
 
   return (
     <React.Fragment>

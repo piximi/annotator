5a514d248df3e0e55ff408bca023dc0853830ae8
Allen Goodman <allen.goodman@icloud.com>
2021-02-22 10:22:53 -0500

    run-length encoding tests

--- src/image/rle.test.ts
+++ src/image/rle.test.ts
@@ -1,11 +1,22 @@
 import { test } from "@jest/globals";
+import { decode, encode } from "./rle";
 
 test("decode", () => {
+  const encoded = [0, 6, 1];
+
+  const decoded = Uint8ClampedArray.from([255, 255, 255, 255, 255, 255, 0]);
+
+  expect(decode(encoded)).toStrictEqual(decoded);
+
   expect(true).toBe(true);
 });
 
 test("encode", () => {
-  expect(true).toBe(true);
+  const decoded = Uint8ClampedArray.from([0, 0, 255, 255, 255, 0, 255]);
+
+  const encoded = [2, 3, 1, 1];
+
+  expect(encode(decoded)).toStrictEqual(encoded);
 });
 
 test("fromString", () => {

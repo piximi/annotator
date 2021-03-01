181e6f9b1bdffe2b71446326d0d656dd0d86e24c
Allen Goodman <allen.goodman@icloud.com>
2021-03-01 11:14:29 -0500

    style fixes

--- src/components/ImageViewer/ImageViewer/ImageViewer.tsx
+++ src/components/ImageViewer/ImageViewer/ImageViewer.tsx
@@ -35,19 +35,42 @@ import { PenSelectionOptions } from "../OperationOptions/PenSelectionOptions";
 
 const theme = createMuiTheme({
   overrides: {
+    MuiDrawer: {
+      paperAnchorDockedLeft: {
+        borderRight: "1px solid rgba(16, 16, 16)",
+      },
+      paperAnchorDockedRight: {
+        borderLeft: "1px solid rgba(16, 16, 16)",
+      },
+    },
+    MuiListItem: {
+      root: {
+        "&$selected": {
+          backgroundColor: "rgba(60, 61, 62gi)",
+        },
+      },
+    },
     MuiSlider: {
       rail: {
-        color: "black",
+        color: "rgba(73, 73, 73)",
       },
       thumb: {
-        color: "yellow",
+        color: "rgba(201, 201, 201)",
       },
       track: {
-        color: "red",
+        color: "rgba(159, 159, 159)",
       },
     },
   },
   palette: {
+    background: {
+      paper: "rgba(40, 40, 40)",
+      default: "rgba(50, 50, 50)",
+    },
+    divider: "rgba(72, 72, 72)",
+    text: {
+      primary: "rgba(190, 190, 190)",
+    },
     type: "dark",
   },
 });

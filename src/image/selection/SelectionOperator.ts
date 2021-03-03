94299712e8582855294275fa4b4dbd0af752cc14
alucas <alicelucas93@gmail.com>
2021-03-03 10:48:45 -0500

    add add() to operator method

--- src/image/selection/SelectionOperator.ts
+++ src/image/selection/SelectionOperator.ts
@@ -6,7 +6,7 @@ import { connectPoints } from "../imageHelper";
 import { simplify } from "../simplify/simplify";
 import { slpf } from "../polygon-fill/slpf";
 import * as uuid from "uuid";
-import { encode } from "../rle";
+import { decode, encode } from "../rle";
 
 export abstract class SelectionOperator {
   image: ImageJS.Image;
@@ -20,6 +20,20 @@ export abstract class SelectionOperator {
     this.image = image;
 
     this.manager = image.getRoiManager();
+  }
+
+  add(oldMask: Array<number>) {
+    if (!this.mask) return;
+
+    const oldMaskData = decode(oldMask);
+    const maskData = decode(this.mask);
+
+    const data = maskData.map((currentValue: number, index: number) => {
+      if (currentValue === 255 || oldMaskData[index] === 255) {
+        return 255;
+      } else return 0;
+    });
+    return encode(data);
   }
 
   abstract get boundingBox(): [number, number, number, number] | undefined;

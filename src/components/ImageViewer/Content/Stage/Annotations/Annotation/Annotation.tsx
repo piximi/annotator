b638d45046874669f3fac1d7641764c9c9b13132
Allen Goodman <allen.goodman@icloud.com>
2021-03-04 14:13:12 -0500

    Annotation

---
+++ src/components/ImageViewer/Content/Stage/Annotations/Annotation/Annotation.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { Category } from "../../../../../../types/Category";
+
+type AnnotationProps = {
+  category: Category;
+  src: string;
+};
+
+export const Annotation = ({ category, src }: AnnotationProps) => {
+  return <React.Fragment />;
+};
---
+++ src/components/ImageViewer/Content/Stage/Annotations/Annotations/Annotations.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { Category } from "../../../../../../types/Category";
+
+type AnnotationsProps = {
+  category: Category;
+  src: string;
+};
+
+export const Annotations = ({ category, src }: AnnotationsProps) => {
+  return <React.Fragment />;
+};

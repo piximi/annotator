229e0f0b3b5133d53d261c752d5d1dae1e3c3929
Allen Goodman <allen.goodman@icloud.com>
2021-03-08 15:49:45 -0500

    protected constructor

--- src/image/Operator/Operator/Operator.ts
+++ src/image/Operator/Operator/Operator.ts
@@ -3,7 +3,7 @@ import * as ImageJS from "image-js";
 export abstract class Operator {
   image: ImageJS.Image;
 
-  constructor(image: ImageJS.Image) {
+  protected constructor(image: ImageJS.Image) {
     this.image = image;
   }
 }

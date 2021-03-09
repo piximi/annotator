21dafab3a51c515cd891770e3bb9eff100eb12e3
Allen Goodman <allen.goodman@icloud.com>
2021-03-09 10:06:40 -0500

    ZoomOperator

--- src/image/Operator/ZoomOperator/ZoomOperator.ts
+++ src/image/Operator/ZoomOperator/ZoomOperator.ts
@@ -9,11 +9,22 @@ export class ZoomOperator extends Operator {
 
   mode: ZoomMode = ZoomMode.In;
 
+  scale: number = 1.0;
+
+  x: number = 0;
+  y: number = 0;
+
+  zooming: boolean = false;
+
   onMouseDown(position: { x: number; y: number }) {}
 
-  onMouseMove(position: { x: number; y: number }) {}
+  onMouseMove(position: { x: number; y: number }) {
+    if (!this.zooming) return;
+  }
 
-  onMouseUp(position: { x: number; y: number }) {}
+  onMouseUp(position: { x: number; y: number }) {
+    if (!this.zooming) return;
+  }
 
   /**
    * Zoom to fit the image to the application window.
@@ -23,5 +34,7 @@ export class ZoomOperator extends Operator {
   /**
    * Zoom the image to its actual size.
    */
-  private reset() {}
+  private reset() {
+    this.scale = 1.0;
+  }
 }

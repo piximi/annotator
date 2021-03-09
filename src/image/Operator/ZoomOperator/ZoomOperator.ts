50742954cd338cbf03ec529ceb9cf5b374f644f2
Allen Goodman <allen.goodman@icloud.com>
2021-03-09 09:51:07 -0500

    events

--- src/image/Operator/ZoomOperator/ZoomOperator.ts
+++ src/image/Operator/ZoomOperator/ZoomOperator.ts
@@ -1,3 +1,10 @@
 import { Operator } from "../Operator";
 
-export class ZoomOperator extends Operator {}
+export class ZoomOperator extends Operator {
+  /**
+   *
+   */
+  onMouseDown(position: { x: number; y: number }): void {}
+  onMouseMove(position: { x: number; y: number }): void {}
+  onMouseUp(position: { x: number; y: number }): void {}
+}

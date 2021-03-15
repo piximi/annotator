200e974f5595a254e3367b041ed78a66ade17019
alucas <alicelucas93@gmail.com>
2021-03-15 10:43:46 -0400

    zoom refactoring -- bug fix

--- src/image/Tool/ZoomTool/ZoomTool.ts
+++ src/image/Tool/ZoomTool/ZoomTool.ts
@@ -96,6 +96,8 @@ export class ZoomTool extends Tool {
   onClick = (event: KonvaEventObject<MouseEvent>) => {
     if (event.evt.button !== 0) return;
 
+    event.evt.preventDefault();
+
     const stage = event.target.getStage();
 
     if (!stage) return;

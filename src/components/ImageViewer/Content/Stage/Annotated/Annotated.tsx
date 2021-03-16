b76ff41456ec5e6d032ac9f93e2ffbc2660216a3
Allen Goodman <allen.goodman@icloud.com>
2021-03-16 15:55:29 -0400

    Annotated

---
+++ src/components/ImageViewer/Content/Stage/Annotated/Annotated.tsx
@@ -0,0 +1,48 @@
+import React, { useEffect, useState } from "react";
+import { SelectionMode } from "../../../../../types/SelectionMode";
+import { Selection } from "../../../../../types/Selection";
+import { SelectedContour } from "../SelectedContour";
+import { AnnotationTool } from "../../../../../image/Tool";
+import { useSelector } from "react-redux";
+import { selectionModeSelector } from "../../../../../store/selectors";
+
+type AnnotatedProps = {
+  annotationTool: AnnotationTool;
+  scale: number;
+  selected: boolean;
+};
+
+export const Annotated = React.forwardRef<
+  React.RefObject<Selection>,
+  AnnotatedProps
+>(({ annotationTool, scale, selected }, ref) => {
+  const selectionMode = useSelector(selectionModeSelector);
+
+  const [points, setPoints] = useState<Array<number>>([]);
+
+  useEffect(() => {
+    if (selectionMode === SelectionMode.New) {
+      if (!annotationTool || !annotationTool.contour) return;
+
+      setPoints(annotationTool.contour);
+    } else {
+      if (!annotationTool.annotating || annotationTool.annotated) return;
+
+      const annotated: React.RefObject<Selection> = ref as React.RefObject<Selection>;
+
+      if (!annotated || !annotated.current) return;
+
+      if (!annotated.current.contour) return;
+
+      setPoints(annotated.current.contour);
+    }
+  }, [annotationTool, ref, selectionMode]);
+
+  return (
+    <React.Fragment>
+      {selected && <SelectedContour points={points} scale={scale} />}
+
+      {ref && <SelectedContour points={points} scale={scale} />}
+    </React.Fragment>
+  );
+});

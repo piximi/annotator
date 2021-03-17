5abf826a1326670337ab6c90e33603a5880207c1
alucas <alicelucas93@gmail.com>
2021-03-17 15:23:05 -0400

    SettingsButton

--- src/components/ImageViewer/ImageViewerAppBar/SettingsButton/SettingButton.tsx
+++ src/components/ImageViewer/ImageViewerAppBar/SettingsButton/SettingButton.tsx
@@ -1,5 +1,25 @@
 import React from "react";
+import Tooltip from "@material-ui/core/Tooltip";
+import Button from "@material-ui/core/Button";
+import SettingsIcon from "@material-ui/icons/Settings";
+import { useStyles } from "../ExportButton/ExportButton.css";
 
 export const SettingsButton = () => {
-  return <React.Fragment />;
+  const classes = useStyles();
+
+  const onClick = () => {};
+
+  return (
+    <React.Fragment>
+      <Tooltip title="Settings">
+        <Button
+          className={classes.button}
+          startIcon={<SettingsIcon />}
+          onClick={onClick}
+        >
+          Settings
+        </Button>
+      </Tooltip>
+    </React.Fragment>
+  );
 };

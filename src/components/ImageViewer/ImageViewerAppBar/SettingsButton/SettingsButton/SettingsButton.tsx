7aad81c1619f746942b64b9fdceda9783c2f8e7d
alucas <alicelucas93@gmail.com>
2021-03-17 15:29:35 -0400

    SettingsButton

--- src/components/ImageViewer/ImageViewerAppBar/SettingsButton/SettingsButton/SettingsButton.tsx
+++ src/components/ImageViewer/ImageViewerAppBar/SettingsButton/SettingsButton/SettingsButton.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import Tooltip from "@material-ui/core/Tooltip";
 import Button from "@material-ui/core/Button";
 import SettingsIcon from "@material-ui/icons/Settings";
-import { useStyles } from "../ExportButton/ExportButton.css";
+import { useStyles } from "../../ExportButton/ExportButton.css";
 
 export const SettingsButton = () => {
   const classes = useStyles();

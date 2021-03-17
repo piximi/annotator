125105dfc30dbd1d65c4d1a59a61673dee812814
alucas <alicelucas93@gmail.com>
2021-03-17 09:34:59 -0400

    useInvertMode custom hook

--- src/hooks/useInvertMode/useInvertMode.ts
+++ src/hooks/useInvertMode/useInvertMode.ts
@@ -1,1 +1,34 @@
-export const useInvertMode = () => {};
+import { useEffect } from "react";
+import { ToolType } from "../../types/ToolType";
+import { Selection as SelectionType } from "../../types/Selection";
+import { useSelector } from "react-redux";
+import {
+  imageInstancesSelector,
+  toolTypeSelector,
+} from "../../store/selectors";
+import { AnnotationTool } from "../../image/Tool/AnnotationTool/AnnotationTool";
+import { selectedAnnotationSelector } from "../../store/selectors/selectedAnnotationSelector";
+
+export const useInvertMode = (annotationTool: AnnotationTool) => {
+  const annotations = useSelector(imageInstancesSelector);
+  const selectedAnnotationId = useSelector(selectedAnnotationSelector);
+
+  useEffect(() => {
+    if (!annotations || !selectedAnnotationId) return;
+
+    const selectedInstance: SelectionType = annotations.filter(
+      (instance: SelectionType) => {
+        return instance.id === selectedAnnotationId;
+      }
+    )[0];
+
+    if (
+      !selectedInstance ||
+      !selectedInstance.mask ||
+      !selectedInstance.contour
+    )
+      return;
+
+    if (annotationTool) return;
+  }, []); //fixme invert mode should be a dependency
+};

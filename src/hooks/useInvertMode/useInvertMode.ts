93122cf239ecae7379d16d50f4e634ea5c0302fb
alucas <alicelucas93@gmail.com>
2021-03-17 10:03:52 -0400

    useInvertMode custom hook

--- src/hooks/useInvertMode/useInvertMode.ts
+++ src/hooks/useInvertMode/useInvertMode.ts
@@ -1,13 +1,22 @@
-import { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { Selection as SelectionType } from "../../types/Selection";
 import { useSelector } from "react-redux";
-import { imageInstancesSelector } from "../../store/selectors";
+import {
+  imageInstancesSelector,
+  invertModeSelector,
+} from "../../store/selectors";
 import { AnnotationTool } from "../../image/Tool/AnnotationTool/AnnotationTool";
 import { selectedAnnotationSelector } from "../../store/selectors/selectedAnnotationSelector";
 
 export const useInvertMode = (annotationTool: AnnotationTool) => {
   const annotations = useSelector(imageInstancesSelector);
+  const invertMode = useSelector(invertModeSelector);
   const selectedAnnotationId = useSelector(selectedAnnotationSelector);
+
+  const [
+    invertedAnnotation,
+    setInvertedAnnotation,
+  ] = useState<SelectionType | null>(null);
 
   useEffect(() => {
     if (!annotations || !selectedAnnotationId) return;
@@ -25,6 +34,31 @@ export const useInvertMode = (annotationTool: AnnotationTool) => {
     )
       return;
 
-    if (annotationTool) return;
-  }, []); //fixme invert mode should be a dependency
+    if (!annotationTool) return;
+
+    const invertedMask = annotationTool.invert(selectedInstance.mask, true);
+
+    const invertedContour = annotationTool.invertContour(
+      selectedInstance.contour
+    );
+
+    const invertedBoundingBox = annotationTool.computeBoundingBoxFromContours(
+      invertedContour
+    );
+
+    annotationTool.mask = invertedMask;
+    annotationTool.boundingBox = invertedBoundingBox;
+    annotationTool.contour = invertedContour;
+
+    const invertedInstance = {
+      ...selectedInstance,
+      boundingBox: invertedBoundingBox,
+      contour: invertedContour,
+      mask: invertedMask,
+    };
+
+    setInvertedAnnotation(invertedInstance);
+  }, [invertMode]);
+
+  return invertedAnnotation;
 };

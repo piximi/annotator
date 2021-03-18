573c6bb98d51dbdbe73547f885ea0ec62b1a724f
Allen Goodman <allen.goodman@icloud.com>
2021-03-18 14:29:08 -0400

    checkboxes

--- src/components/ImageViewer/ToolOptions/Channels/Channels.tsx
+++ src/components/ImageViewer/ToolOptions/Channels/Channels.tsx
@@ -2,19 +2,27 @@ import React from "react";
 import List from "@material-ui/core/List";
 import ListItem from "@material-ui/core/ListItem";
 import ListItemText from "@material-ui/core/ListItemText";
+import { Checkbox, ListItemIcon } from "@material-ui/core";
 
 export const Channels = () => {
   return (
     <List component="nav">
       <ListItem button>
+        <ListItemIcon>
+          <Checkbox checked disableRipple edge="start" tabIndex={-1} />
+        </ListItemIcon>
+
         <ListItemText primary="Red" />
       </ListItem>
 
       <ListItem button>
+        <Checkbox checked disableRipple edge="start" tabIndex={-1} />
+
         <ListItemText primary="Green" />
       </ListItem>
 
       <ListItem button>
+        <Checkbox checked disableRipple edge="start" tabIndex={-1} />
         <ListItemText primary="Blue" />
       </ListItem>
     </List>

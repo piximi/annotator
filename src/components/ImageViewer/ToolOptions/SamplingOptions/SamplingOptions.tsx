e9ea689eb834ffb739ee2877ac6a0afd9f4db834
Allen Goodman <allen.goodman@icloud.com>
2021-03-18 12:00:57 -0400

    sample checkbox

--- src/components/ImageViewer/ToolOptions/SamplingOptions/SamplingOptions.tsx
+++ src/components/ImageViewer/ToolOptions/SamplingOptions/SamplingOptions.tsx
@@ -1,5 +1,20 @@
 import React from "react";
+import Checkbox from "@material-ui/core/Checkbox";
+import FormControlLabel from "@material-ui/core/FormControlLabel";
+
+const SampleLayersCheckbox = () => {
+  return (
+    <FormControlLabel
+      control={<Checkbox name="sample-layers" />}
+      label="Sample all channels?"
+    />
+  );
+};
 
 export const SamplingOptions = () => {
-  return <div />;
+  return (
+    <React.Fragment>
+      <SampleLayersCheckbox />
+    </React.Fragment>
+  );
 };

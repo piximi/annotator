972ce0b37ba1cf2202c5ca3e64d2987623996521
Allen Goodman <allen.goodman@icloud.com>
2021-03-19 16:46:06 -0400

    AnnotationModeTooltip css scaffolding

---
+++ src/components/ImageViewer/ToolOptions/AnnotationMode/AnnotationModeTooltip/AnnotationModeTooltip.css.ts
@@ -0,0 +1,11 @@
+import { makeStyles } from "@material-ui/core/styles";
+
+export const useStyles = makeStyles((theme) => ({
+  tooltip: {
+    backgroundColor: "#f5f5f9",
+    border: "1px solid #dadde9",
+    color: "rgba(0, 0, 0, 0.87)",
+    fontSize: theme.typography.pxToRem(12),
+    maxWidth: 220,
+  },
+}));
--- src/components/ImageViewer/ToolOptions/AnnotationMode/AnnotationModeTooltip/AnnotationModeTooltip.tsx
+++ src/components/ImageViewer/ToolOptions/AnnotationMode/AnnotationModeTooltip/AnnotationModeTooltip.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Tooltip from "@material-ui/core/Tooltip";
 import { Box } from "@material-ui/core";
+import { useStyles } from "./AnnotationModeTooltip.css";
 
 type AnnotationModeTooltipProps = {
   children: React.ReactElement;
@@ -11,15 +12,10 @@ export const AnnotationModeTooltip = ({
   children,
   content,
 }: AnnotationModeTooltipProps) => {
+  const classes = useStyles();
+
   return (
-    <Tooltip
-      title={
-        <React.Fragment>
-          <Box>{content}</Box>
-        </React.Fragment>
-      }
-      placement="bottom"
-    >
+    <Tooltip classes={classes} title={<Box>{content}</Box>} placement="bottom">
       {children}
     </Tooltip>
   );

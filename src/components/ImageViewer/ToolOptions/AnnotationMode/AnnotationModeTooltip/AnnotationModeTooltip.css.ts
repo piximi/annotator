7043b54d8f2fbd8b1f1295883e15ee3009bb7b6e
Allen Goodman <allen.goodman@icloud.com>
2021-03-19 17:05:13 -0400

    example tooltip content

--- src/components/ImageViewer/ToolOptions/AnnotationMode/AnnotationModeTooltip/AnnotationModeTooltip.css.ts
+++ src/components/ImageViewer/ToolOptions/AnnotationMode/AnnotationModeTooltip/AnnotationModeTooltip.css.ts
@@ -2,10 +2,11 @@ import { makeStyles } from "@material-ui/core/styles";
 
 export const useStyles = makeStyles((theme) => ({
   tooltip: {
-    backgroundColor: "#f5f5f9",
+    // backgroundColor: "#f5f5f9",
     border: "1px solid #dadde9",
-    color: "rgba(0, 0, 0, 0.87)",
-    fontSize: theme.typography.pxToRem(12),
-    maxWidth: 220,
+    // color: "rgba(0, 0, 0, 0.87)",
+    // fontSize: theme.typography.pxToRem(12),
+    marginLeft: -100,
+    width: 300,
   },
 }));

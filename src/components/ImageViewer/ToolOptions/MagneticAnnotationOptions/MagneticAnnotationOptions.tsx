b272e0dd8e53244fbed017ec749d80e56f4042de
Allen Goodman <allen.goodman@icloud.com>
2021-03-22 13:36:25 -0400

    tool options

---
+++ src/components/ImageViewer/ToolOptions/MagneticAnnotationOptions/MagneticAnnotationOptions.tsx
@@ -0,0 +1,48 @@
+import Divider from "@material-ui/core/Divider";
+import List from "@material-ui/core/List";
+import ListItem from "@material-ui/core/ListItem";
+import ListItemIcon from "@material-ui/core/ListItemIcon";
+import ListItemText from "@material-ui/core/ListItemText";
+import React from "react";
+import SvgIcon from "@material-ui/core/SvgIcon";
+import { ReactComponent as InvertSelectionIcon } from "../../../icons/InvertAnnotation.svg";
+import { useDispatch, useSelector } from "react-redux";
+import { invertModeSelector } from "../../../../store/selectors";
+import { slice } from "../../../../store";
+import { Typography } from "@material-ui/core";
+import { SampleList } from "../SampleList";
+import { AnnotationMode } from "../AnnotationMode";
+
+export const MagneticAnnotationOptions = () => {
+  const dispatch = useDispatch();
+
+  const invertMode = useSelector(invertModeSelector);
+
+  const onInvertClick = () => {
+    dispatch(slice.actions.setInvertMode({ invertMode: !invertMode }));
+  };
+
+  return (
+    <React.Fragment>
+      <AnnotationMode />
+
+      <Divider />
+
+      <List>
+        <ListItem button onClick={onInvertClick} dense>
+          <ListItemIcon>
+            <SvgIcon>
+              <InvertSelectionIcon />
+            </SvgIcon>
+          </ListItemIcon>
+
+          <ListItemText primary="Invert annotation" />
+        </ListItem>
+      </List>
+
+      <Divider />
+
+      <SampleList />
+    </React.Fragment>
+  );
+};

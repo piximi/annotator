9749c32eb83ace52684a1ea047e65df72b60be59
Allen Goodman <allen.goodman@icloud.com>
2021-03-22 15:38:49 -0400

    SubtractTooltip

--- src/components/ImageViewer/ToolOptions/AnnotationMode/SubtractTooltip/SubtractTooltip.tsx
+++ src/components/ImageViewer/ToolOptions/AnnotationMode/SubtractTooltip/SubtractTooltip.tsx
@@ -1,16 +1,35 @@
 import React from "react";
 import { AnnotationModeTooltip } from "../AnnotationModeTooltip";
+import Typography from "@material-ui/core/Typography";
 
 type SubtractTooltipProps = {
   children: React.ReactElement;
 };
 
 export const SubtractTooltip = ({ children }: SubtractTooltipProps) => {
+  const content = (
+    <React.Fragment>
+      <Typography variant="subtitle2" gutterBottom>
+        Subtract from an existing annotation
+      </Typography>
+
+      <p>
+        Subtracting an area removes the area you label from an existing
+        annotation.
+      </p>
+
+      <p>TODO: add animated gif</p>
+
+      <p>To subtract from an existing selection, do one of the following:</p>
+
+      <ol>
+        <li>Change the annotation mode to “Subtract area”</li>
+        <li>Hold down the Option ⌥ key before starting to annotate.</li>
+      </ol>
+    </React.Fragment>
+  );
+
   return (
-    <AnnotationModeTooltip
-      content={<p>Subtract area from the selected annotation.</p>}
-    >
-      {children}
-    </AnnotationModeTooltip>
+    <AnnotationModeTooltip content={content}>{children}</AnnotationModeTooltip>
   );
 };

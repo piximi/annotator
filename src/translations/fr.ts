f9f61bb19eea049e8c7b8ac38db91da9de0d7dce
alucas <alicelucas93@gmail.com>
2021-03-23 13:51:50 -0400

    traductions francaises

---
+++ src/translations/fr.ts
@@ -0,0 +1,37 @@
+import { language } from "./language";
+
+const fr: language = {
+  translation: {
+    Cancel: "Annuler",
+    Categories: "Catégories",
+    "Create category": "Nouvelle catégorie",
+    "Create new classifier": "Nouveau classifieur",
+    "Delete category": "Supprimer catégorie",
+    "Delete images": "Supprimer images",
+    Description: "Description",
+    Edit: "Modifier",
+    "Edit category": "Modifier catégorie",
+    Help: "Aide",
+    "Hide other categories": "Cacher autres catégories",
+    "Hide sidebar": "Cacher sidebar",
+    "Import images": "Importer images",
+    Logo: "Logo",
+    Model: "Modèle",
+    "Open classifier": "Ouvrir classifier",
+    "Open example classifier": "Ouvrir example classifieur",
+    "Open weights": "Ouvrir poids",
+    Open: "Ouvrir",
+    "Run classifier": "Initier classifieur",
+    "Save annotations and predictions": "Sauver annotations et predictions",
+    "Save classifier": "Sauver classifieur",
+    "Save weights": "Sauver poids",
+    Save: "Sauver",
+    "Search images": "Rechercher images",
+    "Send feedback": "Envoyer feedback",
+    Settings: "Préférences",
+    "Show sidebar": "Montrer sidebar",
+    Unknown: "Inconnu",
+  },
+};
+
+export default fr;

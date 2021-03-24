eb0fbc56a9985d86091074028ed3a02c6d003562
Allen Goodman <allen.goodman@icloud.com>
2021-03-24 13:32:56 -0400

    fas

--- src/translations/index.ts
+++ src/translations/index.ts
@@ -1,9 +1,10 @@
 import de from "./de";
 import en from "./en";
+import fas from "./fas";
 import fi from "./fi";
+import gr from "./gr";
 import hi from "./hi";
 import hu from "./hu";
 import se from "./se";
-import gr from "./gr";
 
-export { de, en, fi, hi, hu, se, gr };
+export { de, en, fas, fi, hi, hu, se, gr };

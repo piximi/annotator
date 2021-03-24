fe9179f01f95b79e51ae66ee58c42be1d008e735
Allen Goodman <allen.goodman@icloud.com>
2021-03-24 12:25:58 -0400

    drag drop context

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,11 +1,15 @@
 import {Provider} from "react-redux";
 import React from "react";
 import {store} from "../src/store";
+import { DndProvider } from "react-dnd";
+import { HTML5Backend } from "react-dnd-html5-backend";
 
 export const decorators = [
   (Story) => (
       <Provider store={store}>
+          <DndProvider backend={HTML5Backend}>
         <Story />
+          </DndProvider>
       </Provider>
   ),
 ];

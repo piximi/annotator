0b885641f77fafe11b2f8269853c63a570f13385
alucas <alicelucas93@gmail.com>
2021-03-26 15:11:03 -0400

    ColorAnnotation for RGB, RGBA, and greyscale images

--- src/image/flood.ts
+++ src/image/flood.ts
@@ -56,28 +56,32 @@ export const makeFloodMap = ({
   y: number;
   image: ImageJS.Image;
 }) => {
-  // if (!image) {
-  //   console.log("Error - No image");
-  //   return;
-  // }
-  // image.target = new Position(x, y);
-  // image.overlayTolerance = -1;
-  // image.overlay = new ImageJS.Image(
-  //   image.width,
-  //   image.height,
-  //   new Uint8ClampedArray(image.width * image.height * 4),
-  //   { alpha: 1 }
-  // );
-
   const tol: Array<number> = [];
 
   const color = image.getPixelXY(x, y);
 
-  for (let i = 0; i < image.data.length; i += image.channels) {
-    const red = Math.abs(image.data[i] - color[0]);
-    const green = Math.abs(image.data[i + 1] - color[1]);
-    const blue = Math.abs(image.data[i + 2] - color[2]);
-    tol.push(Math.floor((red + green + blue) / 3));
+  if (image.data.length === image.width * image.height * 3) {
+    //RGB image
+    for (let i = 0; i < image.data.length; i += 3) {
+      const red = Math.abs(image.data[i] - color[0]);
+      const green = Math.abs(image.data[i + 1] - color[1]);
+      const blue = Math.abs(image.data[i + 2] - color[2]);
+      tol.push(Math.floor((red + green + blue) / 3));
+    }
+  } else if (image.data.length === image.width * image.height * 4) {
+    //RGBA image
+    for (let i = 0; i < image.data.length; i += 4) {
+      const red = Math.abs(image.data[i] - color[0]);
+      const green = Math.abs(image.data[i + 1] - color[1]);
+      const blue = Math.abs(image.data[i + 2] - color[2]);
+      tol.push(Math.floor((red + green + blue) / 3));
+    }
+  } else if (image.data.length === image.width * image.height) {
+    //gresycale
+    for (let i = 0; i < image.data.length; i++) {
+      const grey = Math.abs(image.data[i] - color[0]);
+      tol.push(Math.floor(grey));
+    }
   }
 
   return new ImageJS.Image(image.width, image.height, tol, {

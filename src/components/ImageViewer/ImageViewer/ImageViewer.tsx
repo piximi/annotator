3f4878034fef8467f001221e1f999b39fa542891
Allen Goodman <allen.goodman@icloud.com>
2021-03-29 12:39:41 -0400

    performance fixes

--- src/components/ImageViewer/ImageViewer/ImageViewer.tsx
+++ src/components/ImageViewer/ImageViewer/ImageViewer.tsx
@@ -1,46 +1,16 @@
 import React, { useCallback, useEffect, useState } from "react";
 import { CssBaseline } from "@material-ui/core";
 import { Image } from "../../../types/Image";
-import { ToolType } from "../../../types/ToolType";
-import {
-  toolTypeSelector,
-  zoomSettingsSelector,
-} from "../../../store/selectors";
-import { useDispatch, useSelector } from "react-redux";
+import { useDispatch } from "react-redux";
 import { ImageViewerAppBar } from "../ImageViewerAppBar";
 import { Categories } from "../Categories";
 import { ToolOptions } from "../ToolOptions";
 import { Tools } from "../Tools";
-import { ZoomOptions } from "../ToolOptions/ZoomOptions";
 import { setImage, slice } from "../../../store";
 import { Content } from "../Content";
 import { ThemeProvider } from "@material-ui/core/styles";
 import { useStyles } from "./ImageViewer.css";
-import {
-  ColorAdjustmentIcon,
-  ColorSelectionIcon,
-  EllipticalSelectionIcon,
-  HandIcon,
-  LassoSelectionIcon,
-  MagneticSelectionIcon,
-  ObjectSelectionIcon,
-  PenSelectionIcon,
-  PolygonalSelectionIcon,
-  QuickSelectionIcon,
-  RectangularSelectionIcon,
-  ZoomIcon,
-} from "../../icons";
 import { theme } from "./theme";
-import { useTranslation } from "react-i18next";
-import { RectangularAnnotationOptions } from "../ToolOptions/RectangularAnnotationOptions";
-import { EllipticalAnnotationOptions } from "../ToolOptions/EllipticalAnnotationOptions";
-import { FreehandAnnotationOptions } from "../ToolOptions/FreehandAnnotationOptions";
-import { LassoAnnotationOptions } from "../ToolOptions/LassoAnnotationOptions";
-import { PolygonalAnnotationOptions } from "../ToolOptions/PolygonalAnnotationOptions";
-import { MagneticAnnotationOptions } from "../ToolOptions/MagneticAnnotationOptions";
-import { ColorAnnotationOptions } from "../ToolOptions/ColorAnnotationOptions";
-import { QuickAnnotationOptions } from "../ToolOptions/QuickAnnotationOptions";
-import { ObjectAnnotationOptions } from "../ToolOptions/ObjectAnnotationOptions";
 import * as ImageJS from "image-js";
 import { Shape } from "../../../types/Shape";
 
@@ -51,127 +21,15 @@ type ImageViewerProps = {
 export const ImageViewer = (props: ImageViewerProps) => {
   const dispatch = useDispatch();
 
-  const zoomSettings = useSelector(zoomSettingsSelector);
-
-  const handleZoomReset = () => {
-    dispatch(
-      slice.actions.setZoomReset({ zoomReset: !zoomSettings.zoomReset })
-    );
-  };
-
-  const { t, i18n } = useTranslation();
-
-  const operations = [
-    {
-      description: "Nam a facilisis velit, sit amet interdum ante. In sodales.",
-      icon: <ColorAdjustmentIcon />,
-      method: ToolType.ColorAdjustment,
-      name: t("Color adjustment"),
-      options: <React.Fragment />,
-    },
-    {
-      description: "Nam a facilisis velit, sit amet interdum ante. In sodales.",
-      icon: <RectangularSelectionIcon />,
-      method: ToolType.RectangularSelection,
-      name: "Rectangular selection",
-      options: <RectangularAnnotationOptions />,
-    },
-    {
-      description: "Nam a facilisis velit, sit amet interdum ante. In sodales.",
-      icon: <EllipticalSelectionIcon />,
-      method: ToolType.EllipticalAnnotation,
-      name: "Elliptical selection",
-      options: <EllipticalAnnotationOptions />,
-    },
-    {
-      description: "Nam a facilisis velit, sit amet interdum ante. In sodales.",
-      icon: <PenSelectionIcon />,
-      method: ToolType.PenAnnotation,
-      name: "Pen selection",
-      options: <FreehandAnnotationOptions />,
-    },
-    {
-      description: "Nam a facilisis velit, sit amet interdum ante. In sodales.",
-      icon: <LassoSelectionIcon />,
-      method: ToolType.LassoAnnotation,
-      name: "Lasso selection",
-      options: <LassoAnnotationOptions />,
-    },
-    {
-      description: "Nam a facilisis velit, sit amet interdum ante. In sodales.",
-      icon: <PolygonalSelectionIcon />,
-      method: ToolType.PolygonalAnnotation,
-      name: "Polygonal selection",
-      options: <PolygonalAnnotationOptions />,
-    },
-    {
-      description: "Nam a facilisis velit, sit amet interdum ante. In sodales.",
-      icon: <MagneticSelectionIcon />,
-      method: ToolType.MagneticAnnotation,
-      name: "Magnetic selection",
-      options: <MagneticAnnotationOptions />,
-    },
-    {
-      description: "Nam a facilisis velit, sit amet interdum ante. In sodales.",
-      icon: <ColorSelectionIcon />,
-      method: ToolType.ColorAnnotation,
-      name: "Color selection",
-      options: <ColorAnnotationOptions />,
-    },
-    {
-      description: "Nam a facilisis velit, sit amet interdum ante. In sodales.",
-      icon: <QuickSelectionIcon />,
-      method: ToolType.QuickAnnotation,
-      name: "Quick selection",
-      options: <QuickAnnotationOptions />,
-    },
-    {
-      description: "Nam a facilisis velit, sit amet interdum ante. In sodales.",
-      icon: <ObjectSelectionIcon />,
-      method: ToolType.ObjectAnnotation,
-      name: "Object selection",
-      options: <ObjectAnnotationOptions />,
-    },
-    {
-      description: "Nam a facilisis velit, sit amet interdum ante. In sodales.",
-      icon: <HandIcon />,
-      method: ToolType.Hand,
-      name: "Hand",
-      options: <React.Fragment />,
-    },
-    {
-      description: "Description of zoom here.",
-      icon: <ZoomIcon />,
-      method: ToolType.Zoom,
-      name: "Zoom",
-      options: <ZoomOptions handleRevert={handleZoomReset} />,
-    },
-    {
-      description: "Description of pointer here.",
-      icon: <ObjectSelectionIcon />,
-      method: ToolType.Pointer,
-      name: "Pointer",
-      options: <React.Fragment />,
-    },
-  ];
-
   useEffect(() => {
     if (props.image) {
       dispatch(slice.actions.setImage({ image: props.image }));
     }
   }, [dispatch, props.image]);
 
-  const activeOperation = useSelector(toolTypeSelector);
-
   const classes = useStyles();
 
-  const [collapsed, setCollapsed] = React.useState(false);
-
-  const handleCollapse = (isCollapsed: boolean) => {
-    setCollapsed(isCollapsed);
-  };
-
-  const [dropped, setDropped] = useState<File[]>([]);
+  const [, setDropped] = useState<File[]>([]);
 
   const onDrop = useCallback(
     (item) => {
@@ -223,15 +81,7 @@ export const ImageViewer = (props: ImageViewerProps) => {
 
         <Content onDrop={onDrop} />
 
-        <ToolOptions
-          settings={
-            operations[
-              operations.findIndex(
-                (operation) => operation.method === activeOperation
-              )
-            ].options
-          }
-        />
+        <ToolOptions />
 
         <Tools />
       </div>
--- src/components/ImageViewer/ToolOptions/ToolOptions/ToolOptions.tsx
+++ src/components/ImageViewer/ToolOptions/ToolOptions/ToolOptions.tsx
@@ -2,13 +2,148 @@ import Divider from "@material-ui/core/Divider";
 import Drawer from "@material-ui/core/Drawer";
 import React from "react";
 import { useStyles } from "./ToolOptions.css";
+import {
+  ColorAdjustmentIcon,
+  ColorSelectionIcon,
+  EllipticalSelectionIcon,
+  HandIcon,
+  LassoSelectionIcon,
+  MagneticSelectionIcon,
+  ObjectSelectionIcon,
+  PenSelectionIcon,
+  PolygonalSelectionIcon,
+  QuickSelectionIcon,
+  RectangularSelectionIcon,
+  ZoomIcon,
+} from "../../../icons";
+import { ToolType } from "../../../../types/ToolType";
+import { RectangularAnnotationOptions } from "../RectangularAnnotationOptions";
+import { EllipticalAnnotationOptions } from "../EllipticalAnnotationOptions";
+import { FreehandAnnotationOptions } from "../FreehandAnnotationOptions";
+import { LassoAnnotationOptions } from "../LassoAnnotationOptions";
+import { PolygonalAnnotationOptions } from "../PolygonalAnnotationOptions";
+import { MagneticAnnotationOptions } from "../MagneticAnnotationOptions";
+import { ColorAnnotationOptions } from "../ColorAnnotationOptions";
+import { QuickAnnotationOptions } from "../QuickAnnotationOptions";
+import { ObjectAnnotationOptions } from "../ObjectAnnotationOptions";
+import { ZoomOptions } from "../ZoomOptions";
+import { useDispatch, useSelector } from "react-redux";
+import {
+  toolTypeSelector,
+  zoomSettingsSelector,
+} from "../../../../store/selectors";
+import { slice } from "../../../../store";
+import { useTranslation } from "react-i18next";
 
-type OperationOptionsProps = {
-  settings: React.ReactNode;
-};
+export const ToolOptions = () => {
+  const dispatch = useDispatch();
 
-export const ToolOptions = ({ settings }: OperationOptionsProps) => {
   const classes = useStyles();
+
+  const { t } = useTranslation();
+
+  const activeOperation = useSelector(toolTypeSelector);
+  const zoomSettings = useSelector(zoomSettingsSelector);
+
+  const handleZoomReset = () => {
+    dispatch(
+      slice.actions.setZoomReset({ zoomReset: !zoomSettings.zoomReset })
+    );
+  };
+
+  const operations = [
+    {
+      description: "Nam a facilisis velit, sit amet interdum ante. In sodales.",
+      icon: <ColorAdjustmentIcon />,
+      method: ToolType.ColorAdjustment,
+      name: t("Color adjustment"),
+      options: <React.Fragment />,
+    },
+    {
+      description: "Nam a facilisis velit, sit amet interdum ante. In sodales.",
+      icon: <RectangularSelectionIcon />,
+      method: ToolType.RectangularSelection,
+      name: "Rectangular selection",
+      options: <RectangularAnnotationOptions />,
+    },
+    {
+      description: "Nam a facilisis velit, sit amet interdum ante. In sodales.",
+      icon: <EllipticalSelectionIcon />,
+      method: ToolType.EllipticalAnnotation,
+      name: "Elliptical selection",
+      options: <EllipticalAnnotationOptions />,
+    },
+    {
+      description: "Nam a facilisis velit, sit amet interdum ante. In sodales.",
+      icon: <PenSelectionIcon />,
+      method: ToolType.PenAnnotation,
+      name: "Pen selection",
+      options: <FreehandAnnotationOptions />,
+    },
+    {
+      description: "Nam a facilisis velit, sit amet interdum ante. In sodales.",
+      icon: <LassoSelectionIcon />,
+      method: ToolType.LassoAnnotation,
+      name: "Lasso selection",
+      options: <LassoAnnotationOptions />,
+    },
+    {
+      description: "Nam a facilisis velit, sit amet interdum ante. In sodales.",
+      icon: <PolygonalSelectionIcon />,
+      method: ToolType.PolygonalAnnotation,
+      name: "Polygonal selection",
+      options: <PolygonalAnnotationOptions />,
+    },
+    {
+      description: "Nam a facilisis velit, sit amet interdum ante. In sodales.",
+      icon: <MagneticSelectionIcon />,
+      method: ToolType.MagneticAnnotation,
+      name: "Magnetic selection",
+      options: <MagneticAnnotationOptions />,
+    },
+    {
+      description: "Nam a facilisis velit, sit amet interdum ante. In sodales.",
+      icon: <ColorSelectionIcon />,
+      method: ToolType.ColorAnnotation,
+      name: "Color selection",
+      options: <ColorAnnotationOptions />,
+    },
+    {
+      description: "Nam a facilisis velit, sit amet interdum ante. In sodales.",
+      icon: <QuickSelectionIcon />,
+      method: ToolType.QuickAnnotation,
+      name: "Quick selection",
+      options: <QuickAnnotationOptions />,
+    },
+    {
+      description: "Nam a facilisis velit, sit amet interdum ante. In sodales.",
+      icon: <ObjectSelectionIcon />,
+      method: ToolType.ObjectAnnotation,
+      name: "Object selection",
+      options: <ObjectAnnotationOptions />,
+    },
+    {
+      description: "Nam a facilisis velit, sit amet interdum ante. In sodales.",
+      icon: <HandIcon />,
+      method: ToolType.Hand,
+      name: "Hand",
+      options: <React.Fragment />,
+    },
+    {
+      description: "Description of zoom here.",
+      icon: <ZoomIcon />,
+      method: ToolType.Zoom,
+      name: "Zoom",
+      options: <ZoomOptions handleRevert={handleZoomReset} />,
+    },
+    {
+      description: "Description of pointer here.",
+      icon: <ObjectSelectionIcon />,
+      method: ToolType.Pointer,
+      name: "Pointer",
+      options: <React.Fragment />,
+    },
+  ];
 
   return (
     <Drawer
@@ -21,7 +156,13 @@ export const ToolOptions = ({ settings }: OperationOptionsProps) => {
 
       <Divider />
 
-      {settings}
+      {
+        operations[
+          operations.findIndex(
+            (operation) => operation.method === activeOperation
+          )
+        ].options
+      }
     </Drawer>
   );
 };

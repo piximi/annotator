0795b3d7668dc6b2271f9ef39e7b9f18891fdf43
Allen Goodman <allen.goodman@icloud.com>
2021-03-30 12:28:09 -0400

    fix width

--- src/components/ImageViewer/Content/Content/Content.css.ts
+++ src/components/ImageViewer/Content/Content/Content.css.ts
@@ -1,7 +1,5 @@
 import { makeStyles } from "@material-ui/core/styles";
 
-const toolOptionsDrawerWidth = 50;
-
 export const useStyles = makeStyles((theme) => ({
   content: {
     backgroundColor: theme.palette.background.default,
@@ -12,7 +10,6 @@ export const useStyles = makeStyles((theme) => ({
   },
   parent: {
     cursor: "crosshair",
-    // width: "100%",
-    width: `calc(100% - ${toolOptionsDrawerWidth}px)`,
+    width: "100%",
   },
 }));

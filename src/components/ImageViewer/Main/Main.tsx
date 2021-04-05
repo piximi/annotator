c591ee7be3a6d594810336b334c16a2259abaf0d
Allen Goodman <allen.goodman@icloud.com>
2021-04-05 13:02:54 -0400

    cleanup

--- src/components/ImageViewer/Main/Main.tsx
+++ src/components/ImageViewer/Main/Main.tsx
@@ -9,10 +9,12 @@ import { KonvaEventObject } from "konva/types/Node";
 import { Provider, useDispatch, useSelector } from "react-redux";
 import {
   stageScaleSelector,
+  toolTypeSelector,
   zoomToolOptionsSelector,
 } from "../../../store/selectors";
 import { setStageScale, store } from "../../../store/";
 import { ZoomModeType } from "../../../types/ZoomModeType";
+import { ToolType } from "../../../types/ToolType";
 
 type ImageProps = {
   height: number;
@@ -82,15 +84,12 @@ const Stage = ({ boundingClientRect }: StageProps) => {
 
   const scale = useSelector(stageScaleSelector);
 
+  const toolType = useSelector(toolTypeSelector);
+
   const imageWidth = 1600 * scale;
   const imageHeight = 1200 * scale;
 
   const dispatch = useDispatch();
-
-  // TODO: What do other applications use?
-
-  // const [imageWidth, setImageWidth] = useState<number>(160);
-  // const [imageHeight, setImageHeight] = useState<number>(120);
 
   const { automaticCentering, mode } = useSelector(zoomToolOptionsSelector);
 
@@ -129,29 +128,24 @@ const Stage = ({ boundingClientRect }: StageProps) => {
   }, [stageWidth, stageHeight, imageWidth, imageHeight]);
 
   const onClick = (event: KonvaEventObject<MouseEvent>) => {
+    if (toolType !== ToolType.Zoom) return;
+
     if (!stageRef || !stageRef.current) return;
 
     if (automaticCentering) {
       zoom(mode === ZoomModeType.In ? 100 : -100);
     }
-    // else {
-    //     const position = stageRef.current.getPointerPosition();
-    // }
   };
 
   const onWheel = (event: KonvaEventObject<WheelEvent>) => {
+    if (toolType !== ToolType.Zoom) return;
+
     event.evt.preventDefault();
 
     if (!stageRef || !stageRef.current) return;
 
     zoom(event.evt.deltaY);
   };
-
-  // useEffect(() => {
-  //   if (!imageRef || !imageRef.current) return;
-  //
-  //   console.info(imageRef.current.scale());
-  // })
 
   return (
     <ReactKonva.Stage

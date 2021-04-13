2714eda7a16533bf6661cae1e594528ccce578a2
Allen Goodman <allen.goodman@icloud.com>
2021-04-13 17:13:32 -0400

    useKeyboardShortcuts

--- src/hooks/useKeyboardShortcuts/useKeyboardShortcuts.tsx
+++ src/hooks/useKeyboardShortcuts/useKeyboardShortcuts.tsx
@@ -1,10 +1,13 @@
 import { useHotkeys } from "../useHotkeys";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { setOperation } from "../../store";
 import { ToolType } from "../../types/ToolType";
+import { toolTypeSelector } from "../../store/selectors";
 
 export const useKeyboardShortcuts = () => {
   const dispatch = useDispatch();
+
+  const toolType = useSelector(toolTypeSelector);
 
   /*
    * Color (W)
@@ -40,4 +43,84 @@ export const useKeyboardShortcuts = () => {
   useHotkeys("z", () => {
     dispatch(setOperation({ operation: ToolType.Zoom }));
   });
+
+  /*
+   * Cycle marquee tools (Shift + M)
+   */
+  useHotkeys(
+    "shift+m",
+    () => {
+      switch (toolType) {
+        case ToolType.EllipticalAnnotation:
+          dispatch(setOperation({ operation: ToolType.RectangularAnnotation }));
+
+          break;
+        case ToolType.RectangularAnnotation:
+          dispatch(setOperation({ operation: ToolType.EllipticalAnnotation }));
+
+          break;
+        default:
+          dispatch(setOperation({ operation: ToolType.RectangularAnnotation }));
+
+          break;
+      }
+    },
+    [toolType]
+  );
+
+  /*
+   * Cycle lasso tools (Shift + L)
+   */
+  useHotkeys(
+    "shift+l",
+    () => {
+      switch (toolType) {
+        case ToolType.LassoAnnotation:
+          dispatch(setOperation({ operation: ToolType.MagneticAnnotation }));
+
+          break;
+        case ToolType.MagneticAnnotation:
+          dispatch(setOperation({ operation: ToolType.PolygonalAnnotation }));
+
+          break;
+        case ToolType.PolygonalAnnotation:
+          dispatch(setOperation({ operation: ToolType.LassoAnnotation }));
+
+          break;
+        default:
+          dispatch(setOperation({ operation: ToolType.LassoAnnotation }));
+
+          break;
+      }
+    },
+    [toolType]
+  );
+
+  /*
+   * Cycle learning-based tools (Shift + W)
+   */
+  useHotkeys(
+    "shift+w",
+    () => {
+      switch (toolType) {
+        case ToolType.ColorAnnotation:
+          dispatch(setOperation({ operation: ToolType.ObjectAnnotation }));
+
+          break;
+        case ToolType.ObjectAnnotation:
+          dispatch(setOperation({ operation: ToolType.QuickAnnotation }));
+
+          break;
+        case ToolType.QuickAnnotation:
+          dispatch(setOperation({ operation: ToolType.ColorAnnotation }));
+
+          break;
+        default:
+          dispatch(setOperation({ operation: ToolType.ColorAnnotation }));
+
+          break;
+      }
+    },
+    [toolType]
+  );
 };

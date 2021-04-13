e949f4e91821ab97a07517fb8fec0a84850b0531
Allen Goodman <allen.goodman@icloud.com>
2021-04-13 18:01:06 -0400

    undo

--- src/store/reducer/reducer.ts
+++ src/store/reducer/reducer.ts
@@ -1,10 +1,9 @@
 import { combineReducers } from "redux";
 import { applicationSlice } from "../slices";
 import { toolOptionsSlice } from "../slices/toolOptionsSlice";
-import undoable from "redux-undo";
 
 const reducers = {
-  state: undoable(applicationSlice.reducer),
+  state: applicationSlice.reducer,
   toolOptions: toolOptionsSlice.reducer,
 };
 

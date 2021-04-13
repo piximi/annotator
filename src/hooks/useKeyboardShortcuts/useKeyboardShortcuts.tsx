7b8810f9f22cbff77666afb56ab9aea98f57c9bd
Allen Goodman <allen.goodman@icloud.com>
2021-04-13 16:31:25 -0400

    useKeyboardShortcuts

--- src/hooks/useKeyboardShortcuts/useKeyboardShortcuts.tsx
+++ src/hooks/useKeyboardShortcuts/useKeyboardShortcuts.tsx
@@ -1,11 +1,43 @@
-import { useSelector } from "react-redux";
-import { annotatingSelector } from "../../store/selectors";
-import { useEffect } from "react";
+import { useHotkeys } from "../useHotkeys";
+import { useDispatch } from "react-redux";
+import { setOperation } from "../../store";
+import { ToolType } from "../../types/ToolType";
 
 export const useKeyboardShortcuts = () => {
-  const annotating = useSelector(annotatingSelector);
+  const dispatch = useDispatch();
 
-  useEffect(() => {
-    if (!annotating) return;
-  }, [annotating]);
+  /*
+   * Color (W)
+   */
+  useHotkeys("w", () => {
+    dispatch(setOperation({ operation: ToolType.ColorAnnotation }));
+  });
+
+  /*
+   * Lasso (L)
+   */
+  useHotkeys("l", () => {
+    dispatch(setOperation({ operation: ToolType.LassoAnnotation }));
+  });
+
+  /*
+   * Pencil (P)
+   */
+  useHotkeys("p", () => {
+    dispatch(setOperation({ operation: ToolType.PenAnnotation }));
+  });
+
+  /*
+   * Rectangular (M)
+   */
+  useHotkeys("m", () => {
+    dispatch(setOperation({ operation: ToolType.RectangularAnnotation }));
+  });
+
+  /*
+   * Zoom (Z)
+   */
+  useHotkeys("z", () => {
+    dispatch(setOperation({ operation: ToolType.Zoom }));
+  });
 };

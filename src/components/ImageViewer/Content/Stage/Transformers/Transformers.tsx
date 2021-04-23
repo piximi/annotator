ea4e639db4a34bb958aba5092bf1b89f08839590
alucas <alicelucas93@gmail.com>
2021-04-23 14:08:59 -0400

    parameters for computing contour resizing

--- src/components/ImageViewer/Content/Stage/Transformers/Transformers.tsx
+++ src/components/ImageViewer/Content/Stage/Transformers/Transformers.tsx
@@ -25,7 +25,9 @@ type TransformersProps = {
 export const Transformers = ({ transformPosition }: TransformersProps) => {
   const selectedAnnotationsIds = useSelector(selectedAnnotationsIdsSelector);
 
-  const [boundBox, setBoundBox] = useState<box>({
+  const [boundBox, setBoundBox] = useState<box | null>(null);
+
+  const [startBox, setStartBox] = useState<box>({
     x: 0,
     y: 0,
     height: 0,
@@ -38,10 +40,14 @@ export const Transformers = ({ transformPosition }: TransformersProps) => {
   if (!selectedAnnotationsIds) return <React.Fragment />;
 
   const boundingBoxFunc = (oldBox: box, newBox: box) => {
+    if (!boundBox) setStartBox(oldBox);
     setBoundBox(newBox);
     return newBox;
   };
 
+  /*
+   * Obtain box coordinates in image space
+   */
   const getRelativeBox = (boundBox: box) => {
     const relativePosition = transformPosition({
       x: boundBox.x,
@@ -53,7 +59,29 @@ export const Transformers = ({ transformPosition }: TransformersProps) => {
       y: relativePosition.y / stageScale,
       height: boundBox.height / stageScale,
       width: boundBox.width / stageScale,
+      rotation: 0,
     };
+  };
+
+  const getResizedContour = () => {
+    if (!boundBox || !startBox) return;
+
+    const relativeBoundBox = getRelativeBox(boundBox);
+    const relativeStartBox = getRelativeBox(startBox);
+
+    if (!relativeBoundBox || !relativeStartBox) return;
+
+    // get necessary parameters for transfromation
+    const scaleX = relativeBoundBox.width / relativeStartBox.width;
+    const scaleY = relativeBoundBox.height / relativeStartBox.height;
+    const centerX = relativeBoundBox.x + relativeBoundBox.width / 2;
+    const centerY = relativeBoundBox.y + relativeBoundBox.height / 2;
+  };
+
+  const onTransformEnd = () => {
+    getResizedContour();
+
+    // change image annotations with new contour
   };
 
   return (
@@ -62,9 +90,8 @@ export const Transformers = ({ transformPosition }: TransformersProps) => {
         return (
           <ReactKonva.Transformer
             boundBoxFunc={boundingBoxFunc}
-            onTransformEnd={() => {
-              console.info(getRelativeBox(boundBox));
-            }}
+            centeredScaling={true}
+            onTransformEnd={onTransformEnd}
             id={"tr-".concat(annotationId)}
           />
         );

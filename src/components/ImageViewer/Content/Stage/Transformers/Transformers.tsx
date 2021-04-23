2df70e04cb4fc06ad7951419085b9e69c22ddc9e
alucas <alicelucas93@gmail.com>
2021-04-23 12:58:02 -0400

    obtain final bounding box coords

--- src/components/ImageViewer/Content/Stage/Transformers/Transformers.tsx
+++ src/components/ImageViewer/Content/Stage/Transformers/Transformers.tsx
@@ -1,17 +1,55 @@
 import * as ReactKonva from "react-konva";
-import React from "react";
+import React, { useState } from "react";
 import { useSelector } from "react-redux";
 import { selectedAnnotationsIdsSelector } from "../../../../../store/selectors/selectedAnnotationsIdsSelector";
+import { stageScaleSelector } from "../../../../../store/selectors";
+
+type box = {
+  x: number;
+  y: number;
+  width: number;
+  height: number;
+  rotation: number;
+};
 
 export const Transformers = () => {
   const selectedAnnotationsIds = useSelector(selectedAnnotationsIdsSelector);
 
+  const [boundBox, setBoundBox] = useState<box>({
+    x: 0,
+    y: 0,
+    height: 0,
+    width: 0,
+    rotation: 0,
+  });
+
+  const stageScale = useSelector(stageScaleSelector);
+
   if (!selectedAnnotationsIds) return <React.Fragment />;
+
+  const boundingBoxFunc = (oldBox: box, newBox: box) => {
+    setBoundBox({
+      x: newBox.x,
+      y: newBox.y,
+      width: newBox.width / stageScale,
+      height: newBox.height / stageScale,
+      rotation: 0,
+    });
+    return newBox;
+  };
 
   return (
     <>
       {selectedAnnotationsIds.map((annotationId, idx) => {
-        return <ReactKonva.Transformer id={"tr-".concat(annotationId)} />;
+        return (
+          <ReactKonva.Transformer
+            boundBoxFunc={boundingBoxFunc}
+            onTransformEnd={() => {
+              console.info(boundBox);
+            }}
+            id={"tr-".concat(annotationId)}
+          />
+        );
       })}
     </>
   );

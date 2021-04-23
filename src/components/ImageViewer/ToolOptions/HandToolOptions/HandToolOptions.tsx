c5c7ddbbaf477dcc6d310749a830d9646fa358ff
alucas <alicelucas93@gmail.com>
2021-04-23 09:14:44 -0400

    issue #181 reset position button

--- src/components/ImageViewer/ToolOptions/HandToolOptions/HandToolOptions.tsx
+++ src/components/ImageViewer/ToolOptions/HandToolOptions/HandToolOptions.tsx
@@ -2,6 +2,9 @@ import React from "react";
 import { InformationBox } from "../InformationBox";
 import Divider from "@material-ui/core/Divider";
 import { useTranslation } from "../../../../hooks/useTranslation";
+import List from "@material-ui/core/List";
+import ListItem from "@material-ui/core/ListItem";
+import ListItemText from "@material-ui/core/ListItemText";
 
 export const HandToolOptions = () => {
   const t = useTranslation();
@@ -11,6 +14,12 @@ export const HandToolOptions = () => {
       <InformationBox description="…" name={t("Hand tool")} />
 
       <Divider />
+
+      <List dense>
+        <ListItem button onClick={() => {}}>
+          <ListItemText>{t("Reset position")}</ListItemText>
+        </ListItem>
+      </List>
     </React.Fragment>
   );
 };

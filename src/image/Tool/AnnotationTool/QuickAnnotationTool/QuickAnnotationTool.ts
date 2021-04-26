cb327ec7a3cffa20dbefa43f95dddb55e34d31dd
alucas <alicelucas93@gmail.com>
2021-04-26 11:06:05 -0400

    QuickSelection remove padding

--- src/image/Tool/AnnotationTool/QuickAnnotationTool/QuickAnnotationTool.ts
+++ src/image/Tool/AnnotationTool/QuickAnnotationTool/QuickAnnotationTool.ts
@@ -89,18 +89,7 @@ export class QuickAnnotationTool extends AnnotationTool {
 
     if (!this.currentMask) return;
 
-    const pad = 1; //we pad here to be robust to the case where ROI is along a border of the image.
-    // Otherwise the isoLine algorithm is going to return the wrong mask. We pad to not have any pixel on the border.
-    // this is just for visualization purposes and does not change the underlying true mask data
-
-    //@ts-ignore
-    const padded = this.currentMask.pad({
-      size: pad,
-      algorithm: "set",
-      color: [0, 0, 0, 0],
-    });
-
-    const greyData = padded.grey();
+    const greyData = this.currentMask.grey();
 
     // @ts-ignore
     const greyMatrix = greyData.getMatrix().data;

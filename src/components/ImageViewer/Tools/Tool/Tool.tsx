021faa62edb14bb99dc87179194d6bf1e0ca5f3c
Allen Goodman <allen.goodman@icloud.com>
2021-04-27 10:58:58 -0400

    tool tooltips

--- src/components/ImageViewer/Tools/Tool/Tool.tsx
+++ src/components/ImageViewer/Tools/Tool/Tool.tsx
@@ -3,22 +3,30 @@ import ListItemIcon from "@material-ui/core/ListItemIcon";
 import React from "react";
 import SvgIcon from "@material-ui/core/SvgIcon";
 import Tooltip from "@material-ui/core/Tooltip";
+import Typography from "@material-ui/core/Typography";
 
-type ImageViewerProps = {
+type TipProps = {
+  name: string;
+};
+
+type ToolProps = {
   children: React.ReactNode;
   name: string;
   onClick: () => void;
   selected: boolean;
 };
 
-export const Tool = ({
-  children,
-  name,
-  onClick,
-  selected,
-}: ImageViewerProps) => {
+const Tip = ({ name }: TipProps) => {
   return (
-    <Tooltip aria-label={name} title={name}>
+    <React.Fragment>
+      <Typography>{name}</Typography>
+    </React.Fragment>
+  );
+};
+
+export const Tool = ({ children, name, onClick, selected }: ToolProps) => {
+  return (
+    <Tooltip aria-label={name} title={<Tip name={name} />}>
       <ListItem button onClick={onClick} selected={selected}>
         <ListItemIcon>
           <SvgIcon fontSize="small">{children}</SvgIcon>

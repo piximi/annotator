756028ea7bee0249cc37af9fb9777b6507190d62
Allen Goodman <allen.goodman@icloud.com>
2021-04-30 14:54:00 -0400

    saveAnnotationsSelector

--- src/store/selectors/saveAnnotationsSelector.ts
+++ src/store/selectors/saveAnnotationsSelector.ts
@@ -1,9 +1,46 @@
 import { HistoryStateType } from "../../types/HistoryStateType";
+import { AnnotationType } from "../../types/AnnotationType";
+import { CategoryType } from "../../types/CategoryType";
 
 export const saveAnnotationsSelector = ({
   state,
 }: {
   state: HistoryStateType;
 }): Array<any> => {
-  return [];
+  const image = state.present.image;
+
+  if (!image) return [];
+
+  const columns = {
+    imageChannels: image.shape.channels,
+    imageChecksum: "",
+    imageFilename: image.name,
+    imageFrames: image.shape.frames,
+    imageHeight: image.shape.height,
+    imageId: image.id,
+    imagePlanes: image.shape.planes,
+    imageWidth: image.shape.width,
+  };
+
+  return image.annotations.map((annotation: AnnotationType) => {
+    const index = state.present.categories.findIndex(
+      (category: CategoryType) => {
+        return category.id === annotation.categoryId;
+      }
+    );
+
+    const category = state.present.categories[index];
+
+    return {
+      ...columns,
+      annotationBoundingBoxHeight: annotation.boundingBox[3],
+      annotationBoundingBoxWidth: annotation.boundingBox[2],
+      annotationBoundingBoxX: annotation.boundingBox[0],
+      annotationBoundingBoxY: annotation.boundingBox[1],
+      annotationCategoryId: category.id,
+      annotationCategoryName: category.name,
+      annotationId: annotation.id,
+      annotationMask: annotation.mask,
+    };
+  });
 };

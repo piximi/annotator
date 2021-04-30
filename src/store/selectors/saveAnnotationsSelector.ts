a6ac194e277957a557aa46706bb225da1ec2e1cd
Allen Goodman <allen.goodman@icloud.com>
2021-04-30 14:36:25 -0400

    saveAnnotationsSelector

---
+++ src/store/selectors/saveAnnotationsSelector.ts
@@ -0,0 +1,9 @@
+import { HistoryStateType } from "../../types/HistoryStateType";
+
+export const saveAnnotationsSelector = ({
+  state,
+}: {
+  state: HistoryStateType;
+}): Array<any> => {
+  return [];
+};

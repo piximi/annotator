6ee47bcd9d69769b000920ed4f13db6df468be81
Allen Goodman <allen.goodman@icloud.com>
2021-04-30 17:29:11 -0400

    upgrade sentry

--- src/index.tsx
+++ src/index.tsx
@@ -5,7 +5,6 @@ import { ImageViewer } from "./components";
 import { Provider } from "react-redux";
 import { store } from "./store";
 import * as Sentry from "@sentry/react";
-import { Integrations } from "@sentry/tracing";
 import "./i18n";
 import { DndProvider } from "react-dnd";
 import { HTML5Backend } from "react-dnd-html5-backend";
@@ -13,9 +12,7 @@ import { HTML5Backend } from "react-dnd-html5-backend";
 Sentry.init({
   dsn:
     "https://756e21ab7263457eab9bc2f65edddc79@o71028.ingest.sentry.io/5668724",
-  integrations: [new Integrations.BrowserTracing()],
   release: "image-viewer@" + process.env.npm_package_version,
-  tracesSampleRate: 1.0,
 });
 
 ReactDOM.render(

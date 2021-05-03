620cafdd614f3f5a2f5e8e8a62bd3ca53aa96215
alucas <alicelucas93@gmail.com>
2021-05-03 12:16:40 -0400

    #180 QuickSelection brush cursor

--- src/components/ImageViewer/ToolOptions/QuickAnnotationOptions/QuickAnnotationOptions.tsx
+++ src/components/ImageViewer/ToolOptions/QuickAnnotationOptions/QuickAnnotationOptions.tsx
@@ -1,13 +1,35 @@
 import Divider from "@material-ui/core/Divider";
-import React from "react";
+import React, { useState } from "react";
 import { SampleList } from "../SampleList";
 import { AnnotationMode } from "../AnnotationMode";
 import { InformationBox } from "../InformationBox";
 import { InvertAnnotation } from "../InvertAnnotation";
 import { useTranslation } from "../../../../hooks/useTranslation";
+import List from "@material-ui/core/List";
+import ListItem from "@material-ui/core/ListItem";
+import ListItemText from "@material-ui/core/ListItemText";
+import Slider from "@material-ui/core/Slider";
+import { quickSelectionBrushSizeSelector } from "../../../../store/selectors/quickSelectionBrushSizeSelector";
+import { applicationSlice } from "../../../../store/slices";
+import { useDispatch, useSelector } from "react-redux";
 
 export const QuickAnnotationOptions = () => {
   const t = useTranslation();
+
+  const quickSelectionBrushSize = useSelector(quickSelectionBrushSizeSelector);
+
+  const [brushSize, setBrushSize] = useState<number>(quickSelectionBrushSize);
+
+  const dispatch = useDispatch();
+
+  const onChange = (event: any, changed: number | number[]) => {
+    setBrushSize(changed as number);
+  };
+
+  const onChangeCommitted = (event: any, changed: number | number[]) => {
+    const payload = { quickSelectionBrushSize: changed as number };
+    dispatch(applicationSlice.actions.setQuickSelectionBrushSize(payload));
+  };
 
   return (
     <React.Fragment>
@@ -18,6 +40,23 @@ export const QuickAnnotationOptions = () => {
       <AnnotationMode />
 
       <Divider />
+
+      <List>
+        <ListItem dense>
+          <ListItemText
+            primary={"Brush size"}
+            secondary={
+              <Slider
+                aria-labelledby="quick-selection-brush-size"
+                min={2}
+                onChange={onChange}
+                onChangeCommitted={onChangeCommitted}
+                value={brushSize}
+              />
+            }
+          />
+        </ListItem>
+      </List>
 
       <InvertAnnotation />
 

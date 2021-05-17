fd3e9834b8906bfc01af295416bc425c7639fb30
alucas <alicelucas93@gmail.com>
2021-05-17 11:39:27 -0400

    imports cleanup

--- src/components/ImageViewer/Content/Stage/Annotations/SelectedAnnotations/SelectedAnnotations.tsx
+++ src/components/ImageViewer/Content/Stage/Annotations/SelectedAnnotations/SelectedAnnotations.tsx
@@ -1,18 +1,9 @@
-import * as ReactKonva from "react-konva";
 import React from "react";
-import { useMarchingAnts } from "../../../../../../hooks";
 import { useSelector } from "react-redux";
-import {
-  categoriesSelector,
-  stageScaleSelector,
-} from "../../../../../../store/selectors";
 import { selectedAnnotationSelector } from "../../../../../../store/selectors/selectedAnnotationSelector";
-import * as _ from "lodash";
-import { CategoryType } from "../../../../../../types/CategoryType";
 import { AnnotationType } from "../../../../../../types/AnnotationType";
 import { selectedAnnotationsSelector } from "../../../../../../store/selectors/selectedAnnotationsSelector";
 import { SelectedAnnotation } from "../SelectedAnnotation/SelectedAnnotation";
-import { Annotation } from "../ConfirmedAnnotations/Annotation";
 
 export const SelectedAnnotations = () => {
   const selectedAnnotation = useSelector(selectedAnnotationSelector);

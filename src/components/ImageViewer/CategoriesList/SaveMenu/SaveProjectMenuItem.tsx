83a201a7b34cb93ea7c8780926a8517137f3aba7
alucas <alicelucas93@gmail.com>
2021-06-04 11:53:24 -0400

    saving progress saving masks in zip

--- src/components/ImageViewer/CategoriesList/SaveMenu/SaveProjectMenuItem.tsx
+++ src/components/ImageViewer/CategoriesList/SaveMenu/SaveProjectMenuItem.tsx
@@ -4,12 +4,19 @@ import ListItemText from "@material-ui/core/ListItemText";
 import { useSelector } from "react-redux";
 import { imageHeightSelector } from "../../../../store/selectors/imageHeightSelector";
 import { imageWidthSelector } from "../../../../store/selectors/imageWidthSelector";
-import { imageInstancesSelector } from "../../../../store/selectors";
+import * as uuid from "uuid";
+import {
+  categoriesSelector,
+  imageInstancesSelector,
+} from "../../../../store/selectors";
 import { decode } from "../../../../image/rle";
 import * as ImageJS from "image-js";
 import { imagesSelector } from "../../../../store/selectors/imagesSelector";
 import { ImageType } from "../../../../types/ImageType";
 import { AnnotationType } from "../../../../types/AnnotationType";
+import JSZip from "jszip";
+import { CategoryType } from "../../../../types/CategoryType";
+import { saveAs } from "file-saver";
 
 type SaveAnnotationsMenuItemProps = {
   popupState: any;
@@ -18,50 +25,83 @@ type SaveAnnotationsMenuItemProps = {
 export const SaveProjectMenuItem = ({
   popupState,
 }: SaveAnnotationsMenuItemProps) => {
-  const imageHeight = useSelector(imageHeightSelector);
-  const imageWidth = useSelector(imageWidthSelector);
   const annotations = useSelector(imageInstancesSelector);
   const images = useSelector(imagesSelector);
+  const categories = useSelector(categoriesSelector);
 
   const onExport = () => {
-    if (!imageWidth || !imageHeight) return;
     if (!annotations) return;
 
-    images.forEach((image: ImageType) => {
-      image.annotations.forEach((annotation: AnnotationType) => {
-        const encoded = annotation.mask;
+    let zip = new JSZip();
 
-        const decoded = decode(encoded);
+    const loopOverImages = () => {
+      return new Promise((resolve, reject) => {
+        images.forEach((current: ImageType) => {
+          current.annotations.forEach((annotation: AnnotationType) => {
+            const encoded = annotation.mask;
 
-        const mask = new ImageJS.Image(imageWidth, imageHeight, decoded, {
-          components: 1,
-          alpha: 0,
+            const decoded = decode(encoded);
+
+            const mask = new ImageJS.Image(
+              current.shape.width,
+              current.shape.height,
+              decoded,
+              {
+                components: 1,
+                alpha: 0,
+              }
+            );
+
+            const uri = mask.toDataURL("image/png", {
+              useCanvas: true,
+            });
+
+            //draw to canvas
+            const canvas = document.createElement("canvas");
+            canvas.width = current.shape.width;
+            canvas.height = current.shape.height;
+
+            const ctx = canvas.getContext("2d");
+
+            if (!ctx) return;
+
+            const categoryName = categories.filter((category: CategoryType) => {
+              return category.id === annotation.categoryId;
+            })[0].name;
+
+            zip.folder(`${categoryName}/${current.name}`);
+            const name = uuid.v4();
+
+            //create image from Data URL
+            const image = new Image(current.shape.width, current.shape.height);
+            image.onload = () => {
+              ctx.drawImage(
+                image,
+                0,
+                0,
+                current.shape.width,
+                current.shape.height
+              );
+              canvas.toBlob((blob) => {
+                if (!blob) return;
+
+                console.info("Saving image in zip");
+                zip.file(`${categoryName}/${current.name}/${name}.png`, blob, {
+                  base64: true,
+                });
+              }, "image/png");
+            };
+            image.crossOrigin = "anonymous";
+            image.src = uri;
+          });
         });
+      });
+    };
 
-        const uri = mask.toDataURL("image/png", {
-          useCanvas: true,
-        });
-
-        //draw to canvas
-        const canvas = document.createElement("canvas");
-        canvas.width = imageWidth;
-        canvas.height = imageHeight;
-
-        const ctx = canvas.getContext("2d");
-
-        if (!ctx) return;
-
-        //create image from Data URL
-        const image = new Image(imageWidth, imageHeight);
-        image.onload = () => {
-          ctx.drawImage(image, 0, 0, imageWidth, imageHeight);
-          canvas.toBlob((blob) => {
-            if (!blob) return;
-            saveAs(blob, "test.png");
-          }, "image/png");
-        };
-        image.crossOrigin = "anonymous";
-        image.src = uri;
+    loopOverImages().then(() => {
+      zip.generateAsync({ type: "blob" }).then((blob) => {
+        console.info("Saving zip file");
+        saveAs(blob, "project.zip");
       });
     });
   };

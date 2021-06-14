b3356704392571660fc124ed490e194b2cb1d457
alucas <alicelucas93@gmail.com>
2021-06-14 10:54:45 -0400

    require keyboard shortcuts for tool selections to use shift

--- src/hooks/useKeyboardShortcuts/useKeyboardShortcuts.tsx
+++ src/hooks/useKeyboardShortcuts/useKeyboardShortcuts.tsx
@@ -34,90 +34,90 @@ export const useKeyboardShortcuts = () => {
   //   dispatch(ActionCreators.jump(-1)); //undo should be allowed only if user did an accidental delete or an accidental modification to the selected annotation
   // });
 
-  /*
-   * Cycle lasso tools (Shift + L)
-   */
-  useHotkeys(
-    "shift+l",
-    () => {
-      switch (toolType) {
-        case ToolType.LassoAnnotation:
-          dispatch(setOperation({ operation: ToolType.MagneticAnnotation }));
+  // /*
+  //  * Cycle lasso tools (Shift + L)
+  //  */
+  // useHotkeys(
+  //   "shift+l",
+  //   () => {
+  //     switch (toolType) {
+  //       case ToolType.LassoAnnotation:
+  //         dispatch(setOperation({ operation: ToolType.MagneticAnnotation }));
+  //
+  //         break;
+  //       case ToolType.MagneticAnnotation:
+  //         dispatch(setOperation({ operation: ToolType.PolygonalAnnotation }));
+  //
+  //         break;
+  //       case ToolType.PolygonalAnnotation:
+  //         dispatch(setOperation({ operation: ToolType.LassoAnnotation }));
+  //
+  //         break;
+  //       default:
+  //         dispatch(setOperation({ operation: ToolType.LassoAnnotation }));
+  //
+  //         break;
+  //     }
+  //   },
+  //   [toolType]
+  // );
 
-          break;
-        case ToolType.MagneticAnnotation:
-          dispatch(setOperation({ operation: ToolType.PolygonalAnnotation }));
+  // /*
+  //  * Cycle marquee tools (Shift + M)
+  //  */
+  // useHotkeys(
+  //   "shift+m",
+  //   () => {
+  //     switch (toolType) {
+  //       case ToolType.EllipticalAnnotation:
+  //         dispatch(setOperation({ operation: ToolType.RectangularAnnotation }));
+  //
+  //         break;
+  //       case ToolType.RectangularAnnotation:
+  //         dispatch(setOperation({ operation: ToolType.EllipticalAnnotation }));
+  //
+  //         break;
+  //       default:
+  //         dispatch(setOperation({ operation: ToolType.RectangularAnnotation }));
+  //
+  //         break;
+  //     }
+  //   },
+  //   [toolType]
+  // );
 
-          break;
-        case ToolType.PolygonalAnnotation:
-          dispatch(setOperation({ operation: ToolType.LassoAnnotation }));
-
-          break;
-        default:
-          dispatch(setOperation({ operation: ToolType.LassoAnnotation }));
-
-          break;
-      }
-    },
-    [toolType]
-  );
-
-  /*
-   * Cycle marquee tools (Shift + M)
-   */
-  useHotkeys(
-    "shift+m",
-    () => {
-      switch (toolType) {
-        case ToolType.EllipticalAnnotation:
-          dispatch(setOperation({ operation: ToolType.RectangularAnnotation }));
-
-          break;
-        case ToolType.RectangularAnnotation:
-          dispatch(setOperation({ operation: ToolType.EllipticalAnnotation }));
-
-          break;
-        default:
-          dispatch(setOperation({ operation: ToolType.RectangularAnnotation }));
-
-          break;
-      }
-    },
-    [toolType]
-  );
-
-  /*
-   * Cycle learning-based tools (Shift + W)
-   */
-  useHotkeys(
-    "shift+w",
-    () => {
-      switch (toolType) {
-        case ToolType.ColorAnnotation:
-          dispatch(setOperation({ operation: ToolType.ObjectAnnotation }));
-
-          break;
-        case ToolType.ObjectAnnotation:
-          dispatch(setOperation({ operation: ToolType.QuickAnnotation }));
-
-          break;
-        case ToolType.QuickAnnotation:
-          dispatch(setOperation({ operation: ToolType.ColorAnnotation }));
-
-          break;
-        default:
-          dispatch(setOperation({ operation: ToolType.ColorAnnotation }));
-
-          break;
-      }
-    },
-    [toolType]
-  );
+  // /*
+  //  * Cycle learning-based tools (Shift + W)
+  //  */
+  // useHotkeys(
+  //   "shift+w",
+  //   () => {
+  //     switch (toolType) {
+  //       case ToolType.ColorAnnotation:
+  //         dispatch(setOperation({ operation: ToolType.ObjectAnnotation }));
+  //
+  //         break;
+  //       case ToolType.ObjectAnnotation:
+  //         dispatch(setOperation({ operation: ToolType.QuickAnnotation }));
+  //
+  //         break;
+  //       case ToolType.QuickAnnotation:
+  //         dispatch(setOperation({ operation: ToolType.ColorAnnotation }));
+  //
+  //         break;
+  //       default:
+  //         dispatch(setOperation({ operation: ToolType.ColorAnnotation }));
+  //
+  //         break;
+  //     }
+  //   },
+  //   [toolType]
+  // );
 
   /*
    * Select arrange tool (V)
    */
-  useHotkeys("s", () => {
+  useHotkeys("shift+s", () => {
     dispatch(setOperation({ operation: ToolType.Pointer }));
   });
 
@@ -137,99 +137,102 @@ export const useKeyboardShortcuts = () => {
   /*
    * Select color tool (W)
    */
-  useHotkeys("c", () => {
+  useHotkeys("shift+c", () => {
     dispatch(setOperation({ operation: ToolType.ColorAnnotation }));
   });
 
   /*
    * Select quick tool (W)
    */
-  useHotkeys("q", () => {
+  useHotkeys("shift+q", () => {
     dispatch(setOperation({ operation: ToolType.QuickAnnotation }));
   });
 
   /*
    * Select hand tool (H)
    */
-  useHotkeys("h", () => {
+  useHotkeys("shift+h", () => {
     dispatch(setOperation({ operation: ToolType.Hand }));
   });
 
   /*
    * Select lasso tool (L)
    */
-  useHotkeys("l", () => {
+  useHotkeys("shift+l", () => {
     dispatch(setOperation({ operation: ToolType.LassoAnnotation }));
   });
 
   /*
    * Select polygonal tool (L)
    */
-  useHotkeys("p", () => {
+  useHotkeys("shift+p", () => {
     dispatch(setOperation({ operation: ToolType.PolygonalAnnotation }));
   });
 
   /*
    * Select magnetic tool (M)
    */
-  useHotkeys("m", () => {
+  useHotkeys("shift+m", () => {
     dispatch(setOperation({ operation: ToolType.MagneticAnnotation }));
   });
 
   /*
    * Select pencil tool (P)
    */
-  useHotkeys("d", () => {
+  useHotkeys("shift+d", () => {
     dispatch(setOperation({ operation: ToolType.PenAnnotation }));
   });
 
   /*
    * Select rectangular tool (R)
    */
-  useHotkeys("r", () => {
+  useHotkeys("shift+r", () => {
     dispatch(setOperation({ operation: ToolType.RectangularAnnotation }));
   });
 
   /*
    * Select elliptical tool (E)
    */
-  useHotkeys("e", () => {
+  useHotkeys("shift+e", () => {
     dispatch(setOperation({ operation: ToolType.EllipticalAnnotation }));
   });
 
   /*
    * Select zoom tool (Z)
    */
-  useHotkeys("z", () => {
+  useHotkeys("shift+z", () => {
     dispatch(setOperation({ operation: ToolType.Zoom }));
   });
 
   /*
    * Select intensity adjustment tool (Z)
    */
-  useHotkeys("i", () => {
+  useHotkeys("shift+i", () => {
     dispatch(setOperation({ operation: ToolType.ColorAdjustment }));
   });
 
-  useHotkeys(
-    "*",
-    () => {
-      if (hotkeys.shift) {
-        if (toolType === ToolType.Pointer) return; //pointer tool has its own shift handler for multiple selections
-        dispatch(
-          applicationSlice.actions.setSelectionMode({
-            selectionMode: AnnotationModeType.Add,
-          })
-        );
-      }
-      if (hotkeys.alt) {
-        dispatch(
-          applicationSlice.actions.setSelectionMode({
-            selectionMode: AnnotationModeType.Subtract,
-          })
-        );
-      }
-    },
-    [toolType]
-  );
+  /*
+   * Key shortcuts for special selection mode
+   * */
+  // useHotkeys(
+  //   "*",
+  //   () => {
+  //     if (hotkeys.shift) {
+  //       if (toolType === ToolType.Pointer) return; //pointer tool has its own shift handler for multiple selections
+  //       dispatch(
+  //         applicationSlice.actions.setSelectionMode({
+  //           selectionMode: AnnotationModeType.Add,
+  //         })
+  //       );
+  //     }
+  //     if (hotkeys.alt) {
+  //       dispatch(
+  //         applicationSlice.actions.setSelectionMode({
+  //           selectionMode: AnnotationModeType.Subtract,
+  //         })
+  //       );
+  //     }
+  //   },
+  //   [toolType]
+  // );
 };
